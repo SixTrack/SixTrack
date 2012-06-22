a3140dcbdfef58a251f63e92182d366b5212c65c
Eric Mcintosh <Eric.Mcintosh@cern.ch>
2012-06-22 12:26:20 +0000

    SixTrack Version: 4.4.40 eric     -- Same SixTrack but new make_six for MacOS     -- Defaults to ifort instead of lf95     -- uname -m instead of fs sysname     -- sse3 instead of sse2 for Macs     -- enable_xp.c/disable_xp.c dummied on ifdef APPLE     -- Comma separated map options for FCLMAP     -- NO static linking for Mac     -- Fixed bug with make_six options and added lf95    McIntosh 22nd June, 2012

--- SixTrack/crlibm/disable_xp.c
+++ SixTrack/crlibm/disable_xp.c
@@ -1,9 +1,23 @@
 /* Initialise the IA32/64 FPU flags from Fortran */
 /* An init function which sets FPU flags when needed */
+
+#ifdef __APPLE__
+
+void disable_xp_(void)
+{
+}
+
+#else
+
 #include <fpu_control.h>
-void disable_xp_(void) {
+
+void disable_xp_(void)
+{
   /* Set FPU flags to use double, not double extended,
      with rounding to nearest */
   short unsigned int cw = (_FPU_DEFAULT & ~_FPU_EXTENDED)|_FPU_DOUBLE;
   _FPU_SETCW(cw);
 }
+
+#endif
+
--- SixTrack/crlibm/enable_xp.c
+++ SixTrack/crlibm/enable_xp.c
@@ -1,9 +1,22 @@
 /* Initialise the IA32/64 FPU flags from Fortran */
 /* An init function which sets FPU flags when needed */
+
+#ifdef __APPLE__
+
+void enable_xp_(void)
+{
+}
+
+#else
+
 #include <fpu_control.h>
-void enable_xp_(void) {
+
+void enable_xp_(void)
+{
   /* Set FPU flags to use not double but extended,
      with rounding to nearest */
   short unsigned int cw = (_FPU_DEFAULT & ~_FPU_DOUBLE)|_FPU_EXTENDED;
   _FPU_SETCW(cw);
 }
+
+#endif
