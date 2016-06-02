1905bff257f1a661dc31e546c8394c16dcae87f4
Vikas Gupta <vikasgupta.rmps@gmail.com>
2016-06-02 06:02:16 +0530

    option parsing outside of makefile using shell script for 1-1 conditions with dummy relations

--- SixTrack/option_resolver.sh
+++ SixTrack/option_resolver.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-declare -a error
+
 defaults=$1
 add=$2
 all=("${defaults[@]}" "${add[@]}")
+
 #simulating map-kinda structure(only bash4.0 natively supports map structure)
 declare -i key=0
+declare -i key1=0
+declare -i i=0
+declare -i k=0
+declare -a dummy_all
+declare -a error
+declare -a all_lasttime
 put()
 {
     key=$key+1
@@ -14,11 +21,24 @@ put()
 
 }
 
+put_not()
+{
+    key1=$key1+1
+    options1=("${options1[@]}" "$1") ;
+    options_needs1=("${options_needs1[@]}" "$2")
+    options_excludes1=("${options_excludes1[@]}" "$3");
+
+}
+
 option_check()
 {   
+      #this checks if required options in "presence" of certain option are present and excluding option are absent
+      #if conflict arises, the option which is in default is removed but if both conflicting option is in add or required by any option in add then gives error 
+
     j="$1";
     needed=("${options_needs[${j}]}")
     excluded=("${options_excludes[${j}]}")
+
     for item in ${needed[@]};do
       if ! echo ${all[@]} | grep -w -q "$item";then
         all=("${all[@]}" "$item")
@@ -29,7 +49,7 @@ option_check()
     for item in ${excluded[@]};do
       if echo ${defaults[@]} | grep -w -q "$item";then
         #code to remove conflicting default item from all provided options
-        declare -a dummy_all    
+       
         for index in ${all[@]};do
           [[ $index != "$item" ]] && dummy_all+=($index)
         done
@@ -37,7 +57,47 @@ option_check()
         unset dummy_all
       fi
       if echo ${add[@]} | grep -w -q "$item";then
-        error=("error: added option $item (either given by user explicitly or required as dependency) is conflicting with ${options[${i}]}")
+        if echo ${defaults[@]} |  grep -w -q "$options[$j]";then
+         for index in ${all[@]};do
+          [[ $index != "$options[$j]" ]] && dummy_all+=($index)
+        done
+        all=("${dummy_all[@]}")
+        unset dummy_all
+        break
+        else
+        error=("added option $item (either given by user explicitly or required as dependency) is incompatible with ${options[${i}]}")
+        fi
+      fi 
+    done
+
+}
+
+option_check1()
+{      # this checks if required options in "absence" of certain option are present and excluding options are absent
+       # if excluding options are in add array, then this gives error
+
+    j="$1";
+    needed1=("${options_needs1[${j}]}")
+    excluded1=("${options_excludes1[${j}]}")
+
+    for item in ${needed1[@]};do
+      if ! echo ${all[@]} | grep -w -q "$item";then
+        all=("${all[@]}" "$item")
+        add=("${add[@]}" "$item")
+      fi
+    done
+
+    for item in ${excluded1[@]};do
+      if echo ${defaults[@]} | grep -w -q "$item";then
+        #code to remove conflicting default item from all provided options
+        for index in ${all[@]};do
+          [[ $index != "$item" ]] && dummy_all+=($index)
+        done
+        all=("${dummy_all[@]}")
+        unset dummy_all
+      fi
+      if echo ${add[@]} | grep -w -q "$item";then
+        error=("added option $item (either given by user explicitly or required as dependency) is incompatible with absence of ${options[${i}]}")
       fi 
     done
 
@@ -49,15 +109,19 @@ option_check()
 
 put "hdf5" "collimat" ""
 put "hugenblz" "" "bignblz"
-put "beamgas" "collimat" "hugenblz bignblz"
+put "beamgas" "" "hugenblz bignblz"
 put "da" "" "collimat cpss bpm"
 put "naglib" "da" ""
+put "cpss" "" "vvector"
 put "collimat" "" "da cpss bpm cr"
 
+# add below the case which are to be followed in absence of certain options
+put_not "collimat" "" ""   
 
-declare -i i=0
-declare -i k=0
-while [ $k -lt 10 ];do                         #running test 10 times just to ensure no new conflict arise in items added in 
+
+while [[ $k -lt 10  && ${all_lasttime[@]} != ${all[@]} ]];do             #running test max 10 times just to ensure no new conflict arise in items added
+unset error
+all_lasttime=${all[@]}
 while [ $i -lt $key ];do                         
  for index in ${all[@]};do
      [[ $index = "${options[$i]}" ]] && option_check "$i"
@@ -65,9 +129,16 @@ done
 i=$i+1 
 done
 i=0
+while [ $i -lt $key1 ];do                         
+ if ! echo ${all[@]} | grep -w -q "${options1[$i]}";then
+  option_check1 "$i"
+ fi
+i=$i+1 
+done
+i=0
 k=$k+1
 done
-if echo ${error} | grep -w -q "error";then
+if echo ${error} | grep -w -q "incompatible";then
 echo ${error}
 else
 echo ${all[@]}
