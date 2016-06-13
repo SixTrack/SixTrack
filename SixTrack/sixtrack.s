387edb270143f2e44b5f03591bb2f3a2e433f2bb
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2016-06-13 15:15:42 +0200

    Bump version number to 4.5.35!

--- SixTrack/sixtrack.s
+++ SixTrack/sixtrack.s
@@ -2,8 +2,8 @@
       character*8 version
       character*10 moddate
       integer itot,ttot
-      data version /'4.5.34'/
-      data moddate /'20.04.2016'/
+      data version /'4.5.35'/
+      data moddate /'13.06.2016'/
 +cd license
 !!SixTrack
 !!
