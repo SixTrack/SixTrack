9fa1d3dd7c62838f3cda33851cac3cef5376e399
Vikas Gupta <vikasgupta.rmps@gmail.com>
2016-08-19 16:08:25 +0530

    some modification to script

--- SixTest/stf_compare/not_stf/old_converter.f
+++ SixTest/stf_compare/not_stf/old_converter.f
@@ -6,14 +6,19 @@
 !--   this requires all the fort.# files and not just fort.90
       character*8 cdate,ctime,progrm
       character*80 sixtit,commen
-      integer i,ifipa,ilapa,itopa,imax,icode,numl,turns
+      integer i,rph,ifipa,ilapa,itopa,imax,icode,numl
       dimension qwc(3),clo(3),clop(3),di0(2),dip0(2)
       dimension ta(6,6)
       double precision dummy,dmmac,dnms,dizu0,dnumlr,sigcor,dpscor
       read(90,iostat=ierro) sixtit,commen,cdate,ctime,                   &
      &progrm,ifipa,ilapa,itopa,icode,numl
+       if(ifipa.ne.ilapa) then
        imax=itopa/2
-       turns=numl
+       rph=2
+       else
+       imax=itopa
+       rph=1
+       endif
       open(unit=13,file='older.dat',form='formatted',status='unknown')
        rewind 90
        do 20 i=1,imax
@@ -35,12 +40,21 @@
      &ta(4,5),ta(4,6), ta(5,1),ta(5,2),ta(5,3),ta(5,4),ta(5,5),ta(5,6), &
      &ta(6,1),ta(6,2),ta(6,3),ta(6,4),ta(6,5),ta(6,6), dmmac,dnms,dizu0,&
      &dnumlr,sigcor,dpscor
- 35   read(91-i,iostat=ierro) ia,ifipa,b,c,d,e,f,g,h,p,ilapa,b,         &
+      if(rph.eq.2) then 
+ 35     read(91-i,iostat=ierro) ia,ifipa,b,c,d,e,f,g,h,p,ilapa,b,       &
      &c1,d1,e1,f1,g1,h1,p1
-      if(ierro.eq.0) then
-      write(13,*) ia,ifipa,b,c,d,e,f,g,h,p,ilapa,                       &
+        if(ierro.eq.0) then
+          write(13,*) ia,ifipa,b,c,d,e,f,g,h,p,ilapa,                   &
      &b,c1,d1,e1,f1,g1,h1,p1
-       goto 35
+          goto 35
+        endif
       endif
+      if(rph.eq.1) then
+ 40     read(91-i,iostat=ierro) ia,ifipa,b,c,d,e,f,g,h,p
+        if(ierro.eq.0) then
+          write(13,*) ia,ifipa,b,c,d,e,f,g,h,p
+          goto 40
+         endif
+       endif
  20   continue
       end program
