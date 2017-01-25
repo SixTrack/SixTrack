49fada9758f94d3bcfd019272a73669daa2dfb29
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-01-25 20:46:53 +0100

    Also build boinc_api_fortran.o + fix misspelling

--- SixTrack/buildLibraries.sh
+++ SixTrack/buildLibraries.sh
@@ -17,7 +17,12 @@ cd boinc
 ./_autosetup
 ./configure --disable-client --disable-server --disable-manager --disable-boinczip
 
-make -j
+make -j 2
+#make # Machines with low memory doesn't like the -j...
+
+cd api
+make boinc_api_fortran.o
+cd ..
 
 cd ..
 
@@ -26,9 +31,9 @@ rm -rf libarchive_build
 mkdir libarchive_build
 cd libarchive_build
 
-cmake -DENABLE_BZip2=ON -DENABLE_ZLIB=ON -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF -DENABLE_CNG=OFF -DENABLE_ICONV=OFF -DEABLE_TEST=OFF -G "Unix Makefiles" ../libarchive -LH
+cmake -DENABLE_BZip2=ON -DENABLE_ZLIB=ON -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF -DENABLE_CNG=OFF -DENABLE_ICONV=OFF -DENABLE_TEST=OFF -G "Unix Makefiles" ../libarchive -LH
 
-#make -j
-make # Machines with low memory doesn't like the -j...
+make -j 2
+#make # Machines with low memory doesn't like the -j...
 
 cd ..
