f7d71de5d19e2b508f9dc46772f113fa88eb5f24
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-01-25 21:40:07 +0100

    Windows needs libiconv.

--- SixTrack/buildLibraries.sh
+++ SixTrack/buildLibraries.sh
@@ -17,8 +17,7 @@ cd boinc
 ./_autosetup
 ./configure --disable-client --disable-server --disable-manager --disable-boinczip
 
-make -j 2
-#make # Machines with low memory doesn't like the -j...
+make -j 4 # Machines with low memory doesn't like an automatic -j
 
 cd api
 make boinc_api_fortran.o
@@ -31,9 +30,14 @@ rm -rf libarchive_build
 mkdir libarchive_build
 cd libarchive_build
 
-cmake -DENABLE_BZip2=ON -DENABLE_ZLIB=ON -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF -DENABLE_CNG=OFF -DENABLE_ICONV=OFF -DENABLE_TEST=OFF -G "Unix Makefiles" ../libarchive -LH
+if [[ $(uname)=!= MINGW* ]]; then
+    #Windows build; use ICONV
+    cmake -DENABLE_BZip2=ON -DENABLE_ZLIB=ON -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF -DENABLE_CNG=OFF -DENABLE_ICONV=ON -DENABLE_TEST=OFF -G "Unix Makefiles" ../libarchive -LH
+else
+    #Normal (UNIX) build.
+    cmake -DENABLE_BZip2=ON -DENABLE_ZLIB=ON -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF -DENABLE_CNG=OFF -DENABLE_ICONV=OFF -DENABLE_TEST=OFF -G "Unix Makefiles" ../libarchive -LH
+fi
 
-make -j 2
-#make # Machines with low memory doesn't like the -j...
+make -j 4 # Machines with low memory doesn't like an automatic -j
 
 cd ..
