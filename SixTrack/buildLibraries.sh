82d6da7991a16218a6744ddc147428973f0783ac
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-01-25 21:47:32 +0100

    Fix stupid syntax error

--- SixTrack/buildLibraries.sh
+++ SixTrack/buildLibraries.sh
@@ -30,7 +30,7 @@ rm -rf libarchive_build
 mkdir libarchive_build
 cd libarchive_build
 
-if [[ $(uname)=!= MINGW* ]]; then
+if [[ $(uname) == MINGW* ]]; then
     #Windows build; use ICONV
     cmake -DENABLE_BZip2=ON -DENABLE_ZLIB=ON -DENABLE_CAT=OFF -DENABLE_CPIO=OFF -DENABLE_EXPAT=OFF -DENABLE_INSTALL=OFF -DENABLE_LIBXML2=OFF -DENABLE_LZMA=OFF -DENABLE_NETTLE=OFF -DENABLE_OPENSSL=OFF -DENABLE_TAR=OFF -DENABLE_CNG=OFF -DENABLE_ICONV=ON -DENABLE_TEST=OFF -G "Unix Makefiles" ../libarchive -LH
 else
