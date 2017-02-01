01da56010d78894a5a0f02f2e820ef17bbaaf72f
James Molson <molson@lal.in2p3.fr>
2017-02-01 12:18:36 +0100

    Check all required input files.

--- SixTest/CheckTestInputForBOINC.sh
+++ SixTest/CheckTestInputForBOINC.sh
@@ -5,10 +5,19 @@ if [ -f ${i}Sixin.zip ]; then
 	mkdir ${i}/zip
 	cp ${i}/Sixin.zip ${i}/zip
 	cd ${i}/zip
+
 	unzip -qq Sixin.zip
+
 	for j in $(ls fort.*); do
 		diff -u ${j} ../${j}
 	done
+
+	if [ -f ../extra_inputs.txt ]; then
+		for k in $(cat ../extra_inputs.txt); do
+			diff -u ${k} ../${k}
+		done
+	fi
+
 	cd ../
 	rm -rf zip
 	cd ../
