0d010e530024ba7d1313b497c9c002d8b50e37fd
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-02-24 10:23:58 +0100

    Bump version to 4.6.8

--- SixTrack/sixtrack.s
+++ SixTrack/sixtrack.s
@@ -2,8 +2,8 @@
       character*8 version
       character*10 moddate
       integer itot,ttot
-      data version /'4.6.7'/
-      data moddate /'22.02.2017'/
+      data version /'4.6.8'/
+      data moddate /'24.02.2017'/
 +cd license
 !!SixTrack
 !!
