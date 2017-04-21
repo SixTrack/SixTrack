e04ad6396599958067e0cdeeba3a2f58c144b631
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-04-21 11:57:24 +0200

    Move +cd crlibco and +cd crcoall into its own .s file, avoiding every .s having it's own copy.

--- SixTrack/sixtrack.s
+++ SixTrack/sixtrack.s
@@ -71,15 +71,6 @@
       integer errno,l1,l2
       integer dtostr
 +ei
-+cd crlibco
-      double precision sin_rn,cos_rn,tan_rn,sinh_rn,cosh_rn,asin_rn,    &
-     &acos_rn,atan_rn,atan2_rn,exp_rn,log_rn,log10_rn
-+cd crcoall
-!     Standard output unit
-!     For CR version, this is the "buffer file" fort.92;
-!     Otherwise write directly to "*" aka iso_fortran_env::output_unit (usually unit 6)
-      integer lout
-      common /crflags/lout
 +cd commtim
       real r1,timestart,timenow
       common /mytimes/timestart
