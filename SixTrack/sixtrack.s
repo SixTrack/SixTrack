d6cef4a176ac619cf257a16e5977d4bcee6b99ef
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-10-03 00:00:14 +0200

    Version 4.7.10:  * DYNK: Rewrite from FixedForm to FreeForm FORTRAN and rename dynk.s to dynk.s90  * DYNK: Allocate memory dynamically  * DYNK: Write only what is needed to the CR files  * DYNK: Various tweaks in parseFUN
    See PR #335

--- SixTrack/sixtrack.s
+++ SixTrack/sixtrack.s
@@ -2,8 +2,8 @@
       character*8 version  !Keep data type in sync with 'cr_version'
       character*10 moddate !Keep data type in sync with 'cr_moddate'
       integer itot,ttot
-      data version /'4.7.9'/
-      data moddate /'20.09.2017'/
+      data version /'4.7.10'/
+      data moddate /'02.10.2017'/
 +cd license
 !!SixTrack
 !!
