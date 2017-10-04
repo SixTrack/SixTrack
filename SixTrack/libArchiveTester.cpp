e1fd6ecfa56d4ff979638531f208013517b98676
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-10-04 21:57:50 +0200

    Make the output of libArchiveTester clearer

--- SixTrack/libArchiveTester.cpp
+++ SixTrack/libArchiveTester.cpp
@@ -10,12 +10,18 @@ int main(int argc, char* argv[]){
   if (argc != 2) {
     cout << "Usage: ./libArchiveTester path-to-zipfile" << endl;
     cout << "It will then print a list of the file contents." << endl;
+    cout << "Afterwards it tries to unzip the archive; this may fail if the ZLIB archive was not correctly linked." << endl;
     return EXIT_FAILURE;
   }
 
+  cout << "Listing the archive:" << endl;
   list_archive(argv[1]);
-  cout << "OK, now inflating!"<<endl;
+
+  cout << endl;
+  
+  cout << "OK, now inflating into folder 'libarchive_test' in current directory!" << endl;
   read_archive(argv[1], "libarchive_test");
 
+  cout << "Done!" << endl;
   return EXIT_SUCCESS;
 }
