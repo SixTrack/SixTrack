0a5093099936cea751eec1469abde2ccb1d3bc8b
James Molson <james.molson@cern.ch>
2017-10-25 11:18:16 +0200

    Add initial support for quad precision floating point math.

--- SixTrack/common_headers.s
+++ SixTrack/common_headers.s
@@ -1,6 +1,89 @@
 !     This file contains variables that are shared between all
 !     parts of the code, such as lout and crlibm functions
-      
+    module floatPrecision
+    use, intrinsic :: iso_fortran_env, only : real32, real64, real128
+
++if 32bitm
+    integer, parameter :: fPrec = real32
++ei
++if 64bitm
+    integer, parameter :: fPrec = real64
++ei
++if 128bitm
+    integer, parameter :: fPrec = real128
++ei
+
+    end module floatPrecision
+
+!add in floating point constants
++cd parnum
++if 32bitm
+      double precision c180e0,c1e1,c1e12,c1e13,c1e15,c1e16,c1e2,c1e3,   &
+     &c1e4,c1e6,c1m1,c1m7,c1m10,c1m12,c1m13,c1m15,c1m18,c1m2,c1m21,     &
+     &c1m24,c1m3,c1m36,c1m38,c1m6,c1m9,c2e3,c4e3,crade,clight,four,half,&
+     &one,pieni,pmae,pmap,three,two,zero
+      parameter(pieni = 1d-38)
+      parameter(zero = 0.0d0,half = 0.5d0,one = 1.0d0)
+      parameter(two = 2.0d0,three = 3.0d0,four = 4.0d0)
+      parameter(c1e1 = 1.0d1,c1e2 = 1.0d2,c1m2 = 1.0d-2)
+      parameter(c1e3 = 1.0d3,c2e3 = 2.0d3,c4e3 = 4.0d3,c1e4 = 1.0d4)
+      parameter(c1e12 = 1.0d12,c1e13 = 1.0d13,c1e15 = 1.0d15,c1e16 =    &
+     &1.0d16)
+      parameter(c180e0 = 180.0d0,c1e6 = 1.0d6)
+      parameter(c1m1 = 1.0d-1,c1m3 = 1.0d-3,c1m6 = 1.0d-6,c1m7 = 1.0d-7)
+      parameter(c1m9 = 1.0d-9,c1m10 = 1.0d-10,c1m12 = 1.0d-12)
+      parameter(c1m13 = 1.0d-13,c1m15 = 1.0d-15)
+      parameter(c1m18 = 1.0d-18,c1m21 = 1.0d-21,c1m24 = 1.0d-24)
+      parameter(c1m36 = 1.0d-36,c1m38 = 1.0d-38)
+!     electron mass from PDG, 2002
+      parameter(pmap = 938.271998d0,pmae = .510998902d0)
+      parameter(crade = 2.817940285d-15, clight = 2.99792458d8)
++ei
++if 64bitm
+      double precision c180e0,c1e1,c1e12,c1e13,c1e15,c1e16,c1e2,c1e3,   &
+     &c1e4,c1e6,c1m1,c1m7,c1m10,c1m12,c1m13,c1m15,c1m18,c1m2,c1m21,     &
+     &c1m24,c1m3,c1m36,c1m38,c1m6,c1m9,c2e3,c4e3,crade,clight,four,half,&
+     &one,pieni,pmae,pmap,three,two,zero
+      parameter(pieni = 1d-38)
+      parameter(zero = 0.0d0,half = 0.5d0,one = 1.0d0)
+      parameter(two = 2.0d0,three = 3.0d0,four = 4.0d0)
+      parameter(c1e1 = 1.0d1,c1e2 = 1.0d2,c1m2 = 1.0d-2)
+      parameter(c1e3 = 1.0d3,c2e3 = 2.0d3,c4e3 = 4.0d3,c1e4 = 1.0d4)
+      parameter(c1e12 = 1.0d12,c1e13 = 1.0d13,c1e15 = 1.0d15,c1e16 =    &
+     &1.0d16)
+      parameter(c180e0 = 180.0d0,c1e6 = 1.0d6)
+      parameter(c1m1 = 1.0d-1,c1m3 = 1.0d-3,c1m6 = 1.0d-6,c1m7 = 1.0d-7)
+      parameter(c1m9 = 1.0d-9,c1m10 = 1.0d-10,c1m12 = 1.0d-12)
+      parameter(c1m13 = 1.0d-13,c1m15 = 1.0d-15)
+      parameter(c1m18 = 1.0d-18,c1m21 = 1.0d-21,c1m24 = 1.0d-24)
+      parameter(c1m36 = 1.0d-36,c1m38 = 1.0d-38)
+!     electron mass from PDG, 2002
+      parameter(pmap = 938.271998d0,pmae = .510998902d0)
+      parameter(crade = 2.817940285d-15, clight = 2.99792458d8)
++ei
++if 128bitm
+      double precision c180e0,c1e1,c1e12,c1e13,c1e15,c1e16,c1e2,c1e3,   &
+     &c1e4,c1e6,c1m1,c1m7,c1m10,c1m12,c1m13,c1m15,c1m18,c1m2,c1m21,     &
+     &c1m24,c1m3,c1m36,c1m38,c1m6,c1m9,c2e3,c4e3,crade,clight,four,half,&
+     &one,pieni,pmae,pmap,three,two,zero
+      parameter(pieni = 1d-38)
+      parameter(zero = 0.0d0,half = 0.5d0,one = 1.0d0)
+      parameter(two = 2.0d0,three = 3.0d0,four = 4.0d0)
+      parameter(c1e1 = 1.0d1,c1e2 = 1.0d2,c1m2 = 1.0d-2)
+      parameter(c1e3 = 1.0d3,c2e3 = 2.0d3,c4e3 = 4.0d3,c1e4 = 1.0d4)
+      parameter(c1e12 = 1.0d12,c1e13 = 1.0d13,c1e15 = 1.0d15,c1e16 =    &
+     &1.0d16)
+      parameter(c180e0 = 180.0d0,c1e6 = 1.0d6)
+      parameter(c1m1 = 1.0d-1,c1m3 = 1.0d-3,c1m6 = 1.0d-6,c1m7 = 1.0d-7)
+      parameter(c1m9 = 1.0d-9,c1m10 = 1.0d-10,c1m12 = 1.0d-12)
+      parameter(c1m13 = 1.0d-13,c1m15 = 1.0d-15)
+      parameter(c1m18 = 1.0d-18,c1m21 = 1.0d-21,c1m24 = 1.0d-24)
+      parameter(c1m36 = 1.0d-36,c1m38 = 1.0d-38)
+!     electron mass from PDG, 2002
+      parameter(pmap = 938.271998d0,pmae = .510998902d0)
+      parameter(crade = 2.817940285d-15, clight = 2.99792458d8)
++ei
+
 +cd crlibco
       double precision sin_rn,cos_rn,tan_rn,sinh_rn,cosh_rn,asin_rn,    &
      &acos_rn,atan_rn,atan2_rn,exp_rn,log_rn,log10_rn
