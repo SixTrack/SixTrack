70eac2f44814f1e0323686a57be95f4b3f95ac04
James Molson <james.molson@cern.ch>
2017-10-28 13:59:45 +0200

    Add more constants and re-order parnum.

--- SixTrack/common_headers.s
+++ SixTrack/common_headers.s
@@ -6,9 +6,12 @@
       real(kind=fPrec) c180e0,c1e1,c1e12,c1e13,c1e15,c1e16,c1e2,c1e3,   &
      &c1e4,c1e6,c1m1,c1m7,c1m10,c1m12,c1m13,c1m15,c1m18,c1m2,c1m21,     &
      &c1m24,c1m3,c1m36,c1m38,c1m6,c1m9,c2e3,c4e3,crade,clight,four,half,&
-     &one,pieni,pmae,pmap,three,two,zero
+     &one,pieni,pmae,pmap,three,two,zero,five,six,seven,eight,nine,c1e9,&
+     &c1e10,c1e7,c1m8
 
+!     changing this will break many tests
       parameter(pieni = 1e-38_fPrec)
+!      parameter(pieni = epsilon(zero))
 
       parameter(zero = 0.0_fPrec)
       parameter(half = 0.5_fPrec)
@@ -16,6 +19,11 @@
       parameter(two = 2.0_fPrec)
       parameter(three = 3.0_fPrec)
       parameter(four = 4.0_fPrec)
+      parameter(five = 5.0_fPrec)
+      parameter(six = 6.0_fPrec)
+      parameter(seven = 7.0_fPrec)
+      parameter(eight = 8.0_fPrec)
+      parameter(nine = 9.0_fPrec)
 
       parameter(c1e1 = 1.0e1_fPrec)
       parameter(c1e2 = 1.0e2_fPrec)
@@ -24,6 +32,9 @@
       parameter(c4e3 = 4.0e3_fPrec)
       parameter(c1e4 = 1.0e4_fPrec)
       parameter(c1e6 = 1.0e6_fPrec)
+      parameter(c1e7 = 1.0e7_fPrec)
+      parameter(c1e9 = 1.0e9_fPrec)
+      parameter(c1e10 = 1.0e10_fPrec)
       parameter(c1e12 = 1.0e12_fPrec)
       parameter(c1e13 = 1.0e13_fPrec)
       parameter(c1e15 = 1.0e15_fPrec)
@@ -35,6 +46,7 @@
       parameter(c1m3 = 1.0e-3_fPrec)
       parameter(c1m6 = 1.0e-6_fPrec)
       parameter(c1m7 = 1.0e-7_fPrec)
+      parameter(c1m8 = 1.0e-8_fPrec)
       parameter(c1m9 = 1.0e-9_fPrec)
       parameter(c1m10 = 1.0e-10_fPrec)
       parameter(c1m12 = 1.0e-12_fPrec)
@@ -46,11 +58,19 @@
       parameter(c1m36 = 1.0e-36_fPrec)
       parameter(c1m38 = 1.0e-38_fPrec)
 
-!     electron mass from PDG, 2002
+!     proton mass (MeV)
       parameter(pmap = 938.271998_fPrec)
+
+!     electron mass from PDG, 2002
+!     electron mass (MeV)
       parameter(pmae = .510998902_fPrec)
+
+!     classical electron radius
       parameter(crade = 2.817940285e-15_fPrec)
+
+!     speed of light
       parameter(clight = 2.99792458e8_fPrec)
+
 
 +cd crlibco
       real(kind=fPrec) sin_rn,cos_rn,tan_rn,sinh_rn,cosh_rn,asin_rn,    &
