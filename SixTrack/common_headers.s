71d43380a1df8aca50adee34abef99cd91766775
James Molson <james.molson@cern.ch>
2017-11-08 16:10:32 +0100

    Make numerical constants in cleaner way.

--- SixTrack/common_headers.s
+++ SixTrack/common_headers.s
@@ -3,60 +3,55 @@
 
 !add in floating point constants
 +cd parnum
-      real(kind=fPrec) c180e0,c1e1,c1e12,c1e13,c1e15,c1e16,c1e2,c1e3,   &
-     &c1e4,c1e6,c1m1,c1m7,c1m10,c1m12,c1m13,c1m15,c1m18,c1m2,c1m21,     &
-     &c1m24,c1m3,c1m36,c1m38,c1m6,c1m9,c2e3,c4e3,four,half,             &
-     &one,pieni,three,two,zero,five,six,seven,eight,nine,c1e9,          &
-     &c1e10,c1e7,c1m8
 
 !     changing this will break many tests
-      parameter(pieni = 1e-38_fPrec)
-!      parameter(pieni = epsilon(zero))
+      real(kind=fPrec), parameter :: pieni = 1e-38_fPrec
+!      real(kind=fPrec), parameter :: pieni = epsilon(zero))
 
-      parameter(zero = 0.0_fPrec)
-      parameter(half = 0.5_fPrec)
-      parameter(one = 1.0_fPrec)
-      parameter(two = 2.0_fPrec)
-      parameter(three = 3.0_fPrec)
-      parameter(four = 4.0_fPrec)
-      parameter(five = 5.0_fPrec)
-      parameter(six = 6.0_fPrec)
-      parameter(seven = 7.0_fPrec)
-      parameter(eight = 8.0_fPrec)
-      parameter(nine = 9.0_fPrec)
+      real(kind=fPrec), parameter :: zero = 0.0_fPrec
+      real(kind=fPrec), parameter :: half = 0.5_fPrec
+      real(kind=fPrec), parameter :: one = 1.0_fPrec
+      real(kind=fPrec), parameter :: two = 2.0_fPrec
+      real(kind=fPrec), parameter :: three = 3.0_fPrec
+      real(kind=fPrec), parameter :: four = 4.0_fPrec
+      real(kind=fPrec), parameter :: five = 5.0_fPrec
+      real(kind=fPrec), parameter :: six = 6.0_fPrec
+      real(kind=fPrec), parameter :: seven = 7.0_fPrec
+      real(kind=fPrec), parameter :: eight = 8.0_fPrec
+      real(kind=fPrec), parameter :: nine = 9.0_fPrec
 
-      parameter(c1e1 = 1.0e1_fPrec)
-      parameter(c1e2 = 1.0e2_fPrec)
-      parameter(c1e3 = 1.0e3_fPrec)
-      parameter(c2e3 = 2.0e3_fPrec)
-      parameter(c4e3 = 4.0e3_fPrec)
-      parameter(c1e4 = 1.0e4_fPrec)
-      parameter(c1e6 = 1.0e6_fPrec)
-      parameter(c1e7 = 1.0e7_fPrec)
-      parameter(c1e9 = 1.0e9_fPrec)
-      parameter(c1e10 = 1.0e10_fPrec)
-      parameter(c1e12 = 1.0e12_fPrec)
-      parameter(c1e13 = 1.0e13_fPrec)
-      parameter(c1e15 = 1.0e15_fPrec)
-      parameter(c1e16 = 1.0e16_fPrec)
-      parameter(c180e0 = 180.0_fPrec)
+      real(kind=fPrec), parameter :: c1e1 = 1.0e1_fPrec
+      real(kind=fPrec), parameter :: c1e2 = 1.0e2_fPrec
+      real(kind=fPrec), parameter :: c1e3 = 1.0e3_fPrec
+      real(kind=fPrec), parameter :: c2e3 = 2.0e3_fPrec
+      real(kind=fPrec), parameter :: c4e3 = 4.0e3_fPrec
+      real(kind=fPrec), parameter :: c1e4 = 1.0e4_fPrec
+      real(kind=fPrec), parameter :: c1e6 = 1.0e6_fPrec
+      real(kind=fPrec), parameter :: c1e7 = 1.0e7_fPrec
+      real(kind=fPrec), parameter :: c1e9 = 1.0e9_fPrec
+      real(kind=fPrec), parameter :: c1e10 = 1.0e10_fPrec
+      real(kind=fPrec), parameter :: c1e12 = 1.0e12_fPrec
+      real(kind=fPrec), parameter :: c1e13 = 1.0e13_fPrec
+      real(kind=fPrec), parameter :: c1e15 = 1.0e15_fPrec
+      real(kind=fPrec), parameter :: c1e16 = 1.0e16_fPrec
+      real(kind=fPrec), parameter :: c180e0 = 180.0_fPrec
 
-      parameter(c1m1 = 1.0e-1_fPrec)
-      parameter(c1m2 = 1.0e-2_fPrec)
-      parameter(c1m3 = 1.0e-3_fPrec)
-      parameter(c1m6 = 1.0e-6_fPrec)
-      parameter(c1m7 = 1.0e-7_fPrec)
-      parameter(c1m8 = 1.0e-8_fPrec)
-      parameter(c1m9 = 1.0e-9_fPrec)
-      parameter(c1m10 = 1.0e-10_fPrec)
-      parameter(c1m12 = 1.0e-12_fPrec)
-      parameter(c1m13 = 1.0e-13_fPrec)
-      parameter(c1m15 = 1.0e-15_fPrec)
-      parameter(c1m18 = 1.0e-18_fPrec)
-      parameter(c1m21 = 1.0e-21_fPrec)
-      parameter(c1m24 = 1.0e-24_fPrec)
-      parameter(c1m36 = 1.0e-36_fPrec)
-      parameter(c1m38 = 1.0e-38_fPrec)
+      real(kind=fPrec), parameter :: c1m1 = 1.0e-1_fPrec
+      real(kind=fPrec), parameter :: c1m2 = 1.0e-2_fPrec
+      real(kind=fPrec), parameter :: c1m3 = 1.0e-3_fPrec
+      real(kind=fPrec), parameter :: c1m6 = 1.0e-6_fPrec
+      real(kind=fPrec), parameter :: c1m7 = 1.0e-7_fPrec
+      real(kind=fPrec), parameter :: c1m8 = 1.0e-8_fPrec
+      real(kind=fPrec), parameter :: c1m9 = 1.0e-9_fPrec
+      real(kind=fPrec), parameter :: c1m10 = 1.0e-10_fPrec
+      real(kind=fPrec), parameter :: c1m12 = 1.0e-12_fPrec
+      real(kind=fPrec), parameter :: c1m13 = 1.0e-13_fPrec
+      real(kind=fPrec), parameter :: c1m15 = 1.0e-15_fPrec
+      real(kind=fPrec), parameter :: c1m18 = 1.0e-18_fPrec
+      real(kind=fPrec), parameter :: c1m21 = 1.0e-21_fPrec
+      real(kind=fPrec), parameter :: c1m24 = 1.0e-24_fPrec
+      real(kind=fPrec), parameter :: c1m36 = 1.0e-36_fPrec
+      real(kind=fPrec), parameter :: c1m38 = 1.0e-38_fPrec
 
 +cd crcoall
 !     Standard output unit
@@ -87,15 +82,13 @@
 
       use floatPrecision
 
-      real(kind=fPrec) crade,clight,pmae,pmap
-
 +if .not.fluka
 !     proton mass (MeV)
-      parameter(pmap = 938.271998_fPrec)
+      real(kind=fPrec), parameter :: pmap = 938.271998_fPrec
 
 !     electron mass from PDG, 2002
 !     electron mass (MeV)
-      parameter(pmae = .510998902_fPrec)
+      real(kind=fPrec), parameter :: pmae = .510998902_fPrec
 +ei
 +if fluka
 !     A.Mereghetti and D.Sinuela Pastor, for the FLUKA Team
@@ -103,72 +96,66 @@
 !     synch masses of proton and electron to values used by FLUKA
 !     inserted in main code by the 'fluka' compilation flag
 !     proton and electron mass from PDG, 2014
-      parameter(pmap = 0.938272046e3_fPrec)
-      parameter(pmae = 0.510998928_fPrec)
+      real(kind=fPrec), parameter :: pmap = 0.938272046e3_fPrec
+      real(kind=fPrec), parameter :: pmae = 0.510998928_fPrec
 +ei
 
 !     classical electron radius
-      parameter(crade = 2.817940285e-15_fPrec)
+      real(kind=fPrec), parameter :: crade = 2.817940285e-15_fPrec
 
 !     speed of light
-      parameter(clight = 2.99792458e8_fPrec)
+      real(kind=fPrec), parameter :: clight = 2.99792458e8_fPrec
 
       end module physical_constants
 
 module numerical_constants
   use floatPrecision
 
-  real(kind=fPrec) c180e0,c1e1,c1e12,c1e13,c1e15,c1e16,c1e2,c1e3,   &
- &c1e4,c1e6,c1m1,c1m7,c1m10,c1m12,c1m13,c1m15,c1m18,c1m2,c1m21,     &
- &c1m24,c1m3,c1m36,c1m38,c1m6,c1m9,c2e3,c4e3,four,half,             &
- &one,pieni,three,two,zero,five,six,seven,eight,nine,c1e9,          &
- &c1e10,c1e7,c1m8
+  real(kind=fPrec), parameter :: pieni = 1e-38_fPrec
 
-  parameter(pieni = 1e-38_fPrec)
+  real(kind=fPrec), parameter :: zero = 0.0_fPrec
+  real(kind=fPrec), parameter :: half = 0.5_fPrec
+  real(kind=fPrec), parameter :: one = 1.0_fPrec
+  real(kind=fPrec), parameter :: two = 2.0_fPrec
+  real(kind=fPrec), parameter :: three = 3.0_fPrec
+  real(kind=fPrec), parameter :: four = 4.0_fPrec
+  real(kind=fPrec), parameter :: five = 5.0_fPrec
+  real(kind=fPrec), parameter :: six = 6.0_fPrec
+  real(kind=fPrec), parameter :: seven = 7.0_fPrec
+  real(kind=fPrec), parameter :: eight = 8.0_fPrec
+  real(kind=fPrec), parameter :: nine = 9.0_fPrec
 
-  parameter(zero = 0.0_fPrec)
-  parameter(half = 0.5_fPrec)
-  parameter(one = 1.0_fPrec)
-  parameter(two = 2.0_fPrec)
-  parameter(three = 3.0_fPrec)
-  parameter(four = 4.0_fPrec)
-  parameter(five = 5.0_fPrec)
-  parameter(six = 6.0_fPrec)
-  parameter(seven = 7.0_fPrec)
-  parameter(eight = 8.0_fPrec)
-  parameter(nine = 9.0_fPrec)
+  real(kind=fPrec), parameter :: c1e1 = 1.0e1_fPrec
+  real(kind=fPrec), parameter :: c1e2 = 1.0e2_fPrec
+  real(kind=fPrec), parameter :: c1e3 = 1.0e3_fPrec
+  real(kind=fPrec), parameter :: c2e3 = 2.0e3_fPrec
+  real(kind=fPrec), parameter :: c4e3 = 4.0e3_fPrec
+  real(kind=fPrec), parameter :: c1e4 = 1.0e4_fPrec
+  real(kind=fPrec), parameter :: c1e6 = 1.0e6_fPrec
+  real(kind=fPrec), parameter :: c1e7 = 1.0e7_fPrec
+  real(kind=fPrec), parameter :: c1e9 = 1.0e9_fPrec
+  real(kind=fPrec), parameter :: c1e10 = 1.0e10_fPrec
+  real(kind=fPrec), parameter :: c1e12 = 1.0e12_fPrec
+  real(kind=fPrec), parameter :: c1e13 = 1.0e13_fPrec
+  real(kind=fPrec), parameter :: c1e15 = 1.0e15_fPrec
+  real(kind=fPrec), parameter :: c1e16 = 1.0e16_fPrec
+  real(kind=fPrec), parameter :: c180e0 = 180.0_fPrec
 
-  parameter(c1e1 = 1.0e1_fPrec)
-  parameter(c1e2 = 1.0e2_fPrec)
-  parameter(c1e3 = 1.0e3_fPrec)
-  parameter(c2e3 = 2.0e3_fPrec)
-  parameter(c4e3 = 4.0e3_fPrec)
-  parameter(c1e4 = 1.0e4_fPrec)
-  parameter(c1e6 = 1.0e6_fPrec)
-  parameter(c1e7 = 1.0e7_fPrec)
-  parameter(c1e9 = 1.0e9_fPrec)
-  parameter(c1e10 = 1.0e10_fPrec)
-  parameter(c1e12 = 1.0e12_fPrec)
-  parameter(c1e13 = 1.0e13_fPrec)
-  parameter(c1e15 = 1.0e15_fPrec)
-  parameter(c1e16 = 1.0e16_fPrec)
-  parameter(c180e0 = 180.0_fPrec)
-
-  parameter(c1m1 = 1.0e-1_fPrec)
-  parameter(c1m2 = 1.0e-2_fPrec)
-  parameter(c1m3 = 1.0e-3_fPrec)
-  parameter(c1m6 = 1.0e-6_fPrec)
-  parameter(c1m7 = 1.0e-7_fPrec)
-  parameter(c1m8 = 1.0e-8_fPrec)
-  parameter(c1m9 = 1.0e-9_fPrec)
-  parameter(c1m10 = 1.0e-10_fPrec)
-  parameter(c1m12 = 1.0e-12_fPrec)
-  parameter(c1m13 = 1.0e-13_fPrec)
-  parameter(c1m15 = 1.0e-15_fPrec)
-  parameter(c1m18 = 1.0e-18_fPrec)
-  parameter(c1m21 = 1.0e-21_fPrec)
-  parameter(c1m24 = 1.0e-24_fPrec)
-  parameter(c1m36 = 1.0e-36_fPrec)
-  parameter(c1m38 = 1.0e-38_fPrec)
+  real(kind=fPrec), parameter :: c1m1 = 1.0e-1_fPrec
+  real(kind=fPrec), parameter :: c1m2 = 1.0e-2_fPrec
+  real(kind=fPrec), parameter :: c1m3 = 1.0e-3_fPrec
+  real(kind=fPrec), parameter :: c1m6 = 1.0e-6_fPrec
+  real(kind=fPrec), parameter :: c1m7 = 1.0e-7_fPrec
+  real(kind=fPrec), parameter :: c1m8 = 1.0e-8_fPrec
+  real(kind=fPrec), parameter :: c1m9 = 1.0e-9_fPrec
+  real(kind=fPrec), parameter :: c1m10 = 1.0e-10_fPrec
+  real(kind=fPrec), parameter :: c1m12 = 1.0e-12_fPrec
+  real(kind=fPrec), parameter :: c1m13 = 1.0e-13_fPrec
+  real(kind=fPrec), parameter :: c1m15 = 1.0e-15_fPrec
+  real(kind=fPrec), parameter :: c1m18 = 1.0e-18_fPrec
+  real(kind=fPrec), parameter :: c1m21 = 1.0e-21_fPrec
+  real(kind=fPrec), parameter :: c1m24 = 1.0e-24_fPrec
+  real(kind=fPrec), parameter :: c1m36 = 1.0e-36_fPrec
+  real(kind=fPrec), parameter :: c1m38 = 1.0e-38_fPrec
 
 end module numerical_constants
