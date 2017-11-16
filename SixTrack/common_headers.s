51a6b3038c23b05e3baadda57fe7d9a8cc20c025
James Molson <james.molson@cern.ch>
2017-11-16 15:28:21 +0100

    Remove parnum cd from common_headers.s

--- SixTrack/common_headers.s
+++ SixTrack/common_headers.s
@@ -1,58 +1,6 @@
 !     This file contains variables that are shared between all
 !     parts of the code, such as lout and crlibm functions
 
-!add in floating point constants
-+cd parnum
-
-!     changing this will break many tests
-      real(kind=fPrec), parameter :: pieni = 1e-38_fPrec
-
-      real(kind=fPrec), parameter :: zero  = 0.0_fPrec
-      real(kind=fPrec), parameter :: half  = 0.5_fPrec
-      real(kind=fPrec), parameter :: one   = 1.0_fPrec
-      real(kind=fPrec), parameter :: two   = 2.0_fPrec
-      real(kind=fPrec), parameter :: three = 3.0_fPrec
-      real(kind=fPrec), parameter :: four  = 4.0_fPrec
-      real(kind=fPrec), parameter :: five  = 5.0_fPrec
-      real(kind=fPrec), parameter :: six   = 6.0_fPrec
-      real(kind=fPrec), parameter :: seven = 7.0_fPrec
-      real(kind=fPrec), parameter :: eight = 8.0_fPrec
-      real(kind=fPrec), parameter :: nine  = 9.0_fPrec
-
-      real(kind=fPrec), parameter :: c1e1   = 1.0e1_fPrec
-      real(kind=fPrec), parameter :: c1e2   = 1.0e2_fPrec
-      real(kind=fPrec), parameter :: c1e3   = 1.0e3_fPrec
-      real(kind=fPrec), parameter :: c2e3   = 2.0e3_fPrec
-      real(kind=fPrec), parameter :: c4e3   = 4.0e3_fPrec
-      real(kind=fPrec), parameter :: c1e4   = 1.0e4_fPrec
-      real(kind=fPrec), parameter :: c1e6   = 1.0e6_fPrec
-      real(kind=fPrec), parameter :: c1e7   = 1.0e7_fPrec
-      real(kind=fPrec), parameter :: c1e9   = 1.0e9_fPrec
-      real(kind=fPrec), parameter :: c1e10  = 1.0e10_fPrec
-      real(kind=fPrec), parameter :: c1e12  = 1.0e12_fPrec
-      real(kind=fPrec), parameter :: c1e13  = 1.0e13_fPrec
-      real(kind=fPrec), parameter :: c1e15  = 1.0e15_fPrec
-      real(kind=fPrec), parameter :: c1e16  = 1.0e16_fPrec
-      real(kind=fPrec), parameter :: c180e0 = 180.0_fPrec
-
-      real(kind=fPrec), parameter :: c1m1  = 1.0e-1_fPrec
-      real(kind=fPrec), parameter :: c1m2  = 1.0e-2_fPrec
-      real(kind=fPrec), parameter :: c1m3  = 1.0e-3_fPrec
-      real(kind=fPrec), parameter :: c1m4  = 1.0e-4_fPrec
-      real(kind=fPrec), parameter :: c1m6  = 1.0e-6_fPrec
-      real(kind=fPrec), parameter :: c1m7  = 1.0e-7_fPrec
-      real(kind=fPrec), parameter :: c1m8  = 1.0e-8_fPrec
-      real(kind=fPrec), parameter :: c1m9  = 1.0e-9_fPrec
-      real(kind=fPrec), parameter :: c1m10 = 1.0e-10_fPrec
-      real(kind=fPrec), parameter :: c1m12 = 1.0e-12_fPrec
-      real(kind=fPrec), parameter :: c1m13 = 1.0e-13_fPrec
-      real(kind=fPrec), parameter :: c1m15 = 1.0e-15_fPrec
-      real(kind=fPrec), parameter :: c1m18 = 1.0e-18_fPrec
-      real(kind=fPrec), parameter :: c1m21 = 1.0e-21_fPrec
-      real(kind=fPrec), parameter :: c1m24 = 1.0e-24_fPrec
-      real(kind=fPrec), parameter :: c1m36 = 1.0e-36_fPrec
-      real(kind=fPrec), parameter :: c1m38 = 1.0e-38_fPrec
-
 +cd crcoall
 !     Standard output unit
 !     For CR version, this is the "buffer file" fort.92;
