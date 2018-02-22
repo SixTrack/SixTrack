c4b8ab90708561b80604058f77d5cc34d59d6b17
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-02-22 23:00:50 +0100

    Added scripts to generate html version of manuals from website

---
+++ Doc/cleanupHTML.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+#
+# This script cleans up the html file(s) necessary for the /SixTrack/web/docs folder on the website.
+# The files are written again as php files with the necessary headers and includes for the website.
+# Written by Veronica Berglyd Olsen, Feb 2018
+#
+
+import sys
+
+from os import path, listdir
+
+def wrapHTML(htmlDir, htmlFile, fileList):
+    
+    phpFile  = htmlFile[0:-5]+".php"
+    htmlBody = ""
+    htmlHead = ""
+    genStr   = ""
+    whereNow = 0
+    
+    print("")
+    print("Loading HTML File ...")
+    
+    with open(path.join(htmlDir,htmlFile),encoding="utf-8") as inFile:
+        for theLine in inFile:
+            
+            theLine = theLine.strip()
+            if theLine == "": continue
+            
+            if theLine[0:6] == "<head>":
+                whereNow = 1
+                continue
+            
+            if theLine[0:7] == "</head>":
+                whereNow = 0
+                continue
+            
+            if theLine[0:8] == "<article":
+                whereNow = 2
+                continue
+            
+            if theLine[0:10] == "</article>":
+                whereNow = 0
+                continue
+            
+            if whereNow == 1:
+                if theLine[0:4] == "<!--":
+                    htmlHead += theLine+"\n"
+                    tmpBits   = theLine[4:-4].split("http://")
+                    if len(tmpBits) == 2:
+                        genStr = tmpBits[0].strip().replace("LaTeXML","<a href='http://"+tmpBits[1]+"'>LaTeXML</a>")
+                if theLine[0:5] == "<link":
+                    htmlHead += theLine+"\n"
+                if theLine[0:7] == "<script":
+                    htmlHead += theLine+"\n"
+            
+            if whereNow == 2:
+                htmlBody += theLine+"\n"
+    
+    print("Done.")
+    print("Writing PHP file ...")
+    
+    with open(path.join(htmlDir,phpFile),"w") as outFile:
+        outFile.write("<?php\n")
+        outFile.write("    $bMain  = true;\n")
+        # outFile.write("    $sTitle = 'Manual';\n")
+        outFile.write("    $sHead  = '"+htmlHead+"';\n")
+        outFile.write("    $sHead .= '<link rel=\"stylesheet\" href=\"/SixTrack/web/css/latexml-fix.css\" type=\"text/css\">\n';\n")
+        outFile.write("    require_once('../../includes/header.php');\n")
+        outFile.write("?>\n")
+        outFile.write("<article id='manual' class='ltx_document'>\n")
+        outFile.write("<div id='generator'>"+genStr+"</div>\n")
+        outFile.write(htmlBody)
+        outFile.write("</article>\n")
+        outFile.write("<aside>\n")
+        outFile.write("    <?php include_once($incPath.'/aside_manual.php'); ?>\n")
+        outFile.write("</aside>\n")
+        outFile.write("<?php\n")
+        outFile.write("    require_once($incPath.'/footer.php');\n")
+        outFile.write("?>\n")
+    
+    print("Done.")
+    
+    return
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 2:
+        htmlDir = sys.argv[1]
+    else:
+        print("Error: Input folder missing.")
+        exit(1)
+    
+    htmlFiles = []
+    for dirItem in listdir(htmlDir):
+        if len(dirItem) < 5: continue
+        if dirItem[-5:] == ".html":
+            htmlFiles.append(dirItem[0:-5])
+    # print(htmlFiles)
+    #for htmlFile in htmlFiles:
+    wrapHTML(htmlDir,"manual.html",htmlFiles)
+    
+    exit(0)
+ 
+# End Main
---
+++ Doc/generateForWeb.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+# This script generates the files necessary for the /SixTrack/web/docs folder on the website.
+# Requires the latexml package to be installed.
+# Written by Veronica Berglyd Olsen, Feb 2018
+#
+
+CURR=$(pwd)
+MUSER=$CURR/user_manual
+MPHYS=$CURR/physics_manual
+MBUILD=$CURR/building_sixtrack
+OUSERF=$CURR/html/user_full
+#OUSERS=$CURR/html/user_split
+OPHYS=$CURR/html/physics_full
+OBUILD=$CURR/html/build_full
+
+# LaTeXML Options
+MATHJAX='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML'
+FORMAT=html5
+
+mkdir -pv $OUSERF
+#mkdir -pv $OUSERS
+mkdir -pv $OPHYS
+mkdir -pv $OBUILD
+
+echo ""
+echo "*******************************"
+echo "* Generating User Manual HTML *"
+echo "*******************************"
+echo ""
+
+cd $MUSER
+make
+cp $MUSER/six.pdf $CURR/html/user_manual.pdf
+latexml six.tex | latexmlpost --dest=$OUSERF/manual.html --format=$FORMAT --javascript=$MATHJAX -
+#latexml six.tex | latexmlpost --dest=$OUSERS/manual.html --format=$FORMAT --javascript=$MATHJAX --splitat=chapter -
+$CURR/cleanupHTML.py $OUSERF
+
+echo ""
+echo "**********************************"
+echo "* Generating Physics Manual HTML *"
+echo "**********************************"
+echo ""
+
+cd $MPHYS
+make
+cp $MPHYS/sixphys.pdf $CURR/html/physics_manual.pdf
+latexml sixphys.tex | latexmlpost --dest=$OPHYS/manual.html --format=$FORMAT --javascript=$MATHJAX -
+$CURR/cleanupHTML.py $OPHYS
+
+echo ""
+echo "********************************"
+echo "* Generating Build Manual HTML *"
+echo "********************************"
+echo ""
+
+cd $MBUILD
+make
+cp $MBUILD/building_sixtrack.pdf $CURR/html/building_sixtrack.pdf
+latexml building_sixtrack.tex | latexmlpost --dest=$OBUILD/manual.html --format=$FORMAT --javascript=$MATHJAX -
+$CURR/cleanupHTML.py $OBUILD
+
+echo ""
+echo "**********"
+echo "*  DONE  *"
+echo "**********"
+echo ""
+echo "The content of the folder 'html' can now be uploaded to /afs/cern.ch/project/sixtrack/web/docs/"
+echo ""
