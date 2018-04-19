0aaab28c9ef7530bbce7c8ce82d3ff905616c4ef
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-04-19 17:11:51 +0200

    Various fixes to strings in file_units and string_tools

--- SixTrack/file_units.f90
+++ SixTrack/file_units.f90
@@ -15,22 +15,23 @@ end module crcoall
 
 ! =================================================================================================
 !  FILE UNITS MODULE
-!  Written by: Veronica Berglyd Olsen, BE-ABP-HSS, March 2018
-!  Last modified: 28-03-2018
+!  V.K. Berglyd Olsen, BE-ABP-HSS, March 2018
+!  Last modified: 2018-04-19
 ! =================================================================================================
 module file_units
   
+  use strings
+  
   implicit none
   
-  integer :: funit_minUnit, funit_maxUnit, funit_maxStrLen
-  parameter(funit_minUnit=1000, funit_maxUnit=1999, funit_maxStrLen=100)
+  integer, public :: funit_minUnit, funit_maxUnit
+  parameter(funit_minUnit=1000, funit_maxUnit=1999)
   
-  integer,                        dimension(:), allocatable :: funit_usedUnits
-  character(len=funit_maxStrLen), dimension(:), allocatable :: funit_usedByFile
-  
-  integer :: funit_nextUnit = funit_minUnit
-  
-  save funit_usedUnits, funit_usedByFile, funit_nextUnit
+  integer,      allocatable, private, save :: funit_usedUnits(:)
+  type(string), allocatable, private, save :: funit_usedByFile(:)
+  logical,                   private, save :: funit_arrInit  = .true.
+  integer,                   private, save :: funit_nUnits   = 0
+  integer,                   private, save :: funit_nextUnit = funit_minUnit
 
 contains
   
@@ -39,54 +40,63 @@ contains
   subroutine funit_requestUnit(fileName, fileUnit)
     
     use crcoall
+    use string_tools
     
     implicit none
     
     character(len=*), intent(in)  :: fileName
     integer,          intent(out) :: fileUnit
+    character(len=:), allocatable :: cleanName
     
-    character(len=len(fileName)) cleanName
+    integer,          allocatable :: tmp_usedUnits(:)
+    type(string),     allocatable :: tmp_usedByFile(:)
     
-    integer ch
     logical isOpen
     
-    ! Check if the subroutine is called for the first time, and initialise the arrays to zero size.
-    if(funit_nextUnit == funit_minUnit) then
-      allocate(funit_usedUnits(0))
-      allocate(funit_usedByFile(0))
-    end if
-    
-    ! Remove \0s from fileName
-    cleanName = fileName
-    do ch=1, len(cleanName)
-      if(cleanName(ch:ch) == char(0)) then
-        cleanName(ch:ch) = " "
-      end if
-    end do
+    cleanName = chr_trim(chr_trimZero(fileName))
     
 10  continue
     
     fileUnit       = funit_nextUnit
     funit_nextUnit = funit_nextUnit + 1
+    
     if(funit_nextUnit > funit_maxUnit) goto 30
+    
+    if(funit_arrInit) then
+      allocate(funit_usedUnits(1))
+      allocate(funit_usedByFile(1))
+      funit_arrInit = .false.
+    else
+      allocate(tmp_usedUnits(funit_nUnits))
+      allocate(tmp_usedByFile(funit_nUnits))
+      tmp_usedUnits(1:funit_nUnits)  = funit_usedUnits(1:funit_nUnits)
+      tmp_usedByFile(1:funit_nUnits) = funit_usedByFile(1:funit_nUnits)
+      deallocate(funit_usedUnits)
+      deallocate(funit_usedByFile)
+      allocate(funit_usedUnits(funit_nUnits+1))
+      allocate(funit_usedByFile(funit_nUnits+1))
+      funit_usedUnits(1:funit_nUnits)  = tmp_usedUnits(1:funit_nUnits)
+      funit_usedByFile(1:funit_nUnits) = tmp_usedByFile(1:funit_nUnits)
+      deallocate(tmp_usedUnits)
+      deallocate(tmp_usedByFile)
+    end if
+    funit_nUnits = funit_nUnits + 1
     
     inquire(unit=fileUnit, opened=isOpen)
     if(isOpen) then 
-      funit_usedUnits  = [funit_usedUnits,  fileUnit]
-      funit_usedByFile = [funit_usedByFile, "Unknown"//repeat(" ",93)]
+      funit_usedUnits(funit_nUnits)  = fileUnit
+      funit_usedByFile(funit_nUnits) = string("Unknown, unit already open.")
       goto 10
     else
-      funit_usedUnits  = [funit_usedUnits,  fileUnit]
-      funit_usedByFile = [funit_usedByFile, cleanName]
+      funit_usedUnits(funit_nUnits)  = fileUnit
+      funit_usedByFile(funit_nUnits) = string(cleanName)
       goto 20
     end if
 
-20  continue
-    ! write(lout,"(a,i4,a,a)") "FUNIT> Unit ",fileUnit," assigned to file: ",trim(cleanName)
-    return
+20  return
     
 30  continue
-    write(lout,"(a)") "FUNIT> ERROR: Failed to find an available file unit for file ",trim(cleanName)
+    write(lout,"(3a)") "FUNIT> ERROR Failed to find an available file unit for file '",cleanName,"'"
     stop -1
     
   end subroutine funit_requestUnit
@@ -102,7 +112,7 @@ contains
     
     write(lout,"(a)") "FUNIT> Dynamically assigned file units are:"
     do i=1, size(funit_usedUnits)
-      write(lout,"(a,i4,a,a)") "FUNIT> Unit ",funit_usedUnits(i)," assigned to file: ",trim(funit_usedByFile(i))
+      write(lout,"(a,i4,2a)") "FUNIT>   Unit ",funit_usedUnits(i)," assigned to: ",funit_usedByFile(i)%chr
     end do
     
   end subroutine funit_listUnits
@@ -124,11 +134,11 @@ contains
       write(lout,*) "Unit ",dumpUnit," was already taken."
       stop -1
     end if
-
+    
     open(dumpUnit,file="file_units.dat",form="formatted")
     write(dumpUnit,"(a)") "# unit  assigned_to"
     do i=1, size(funit_usedUnits)
-      write(dumpUnit,"(i6,a,a)") funit_usedUnits(i),"  ",trim(funit_usedByFile(i))
+      write(dumpUnit,"(i6,2a)") funit_usedUnits(i),"  ",funit_usedByFile(i)%chr
     end do
     close(dumpUnit)
     
