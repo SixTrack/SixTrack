a30d4ebcf8aa1aebedd151707b2c05692ac94d34
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-05-23 00:34:58 +0200

    Made counting of element number in SING the same as for BLOC and STRU

--- SixTrack/sixtrack_input.f90
+++ SixTrack/sixtrack_input.f90
@@ -123,14 +123,14 @@ subroutine sixin_blockReport
   
 end subroutine sixin_blockReport
 
-subroutine sixin_parseInputLineSING(inLine, iElem, iErr)
+subroutine sixin_parseInputLineSING(inLine, iLine, iErr)
   
   use parpro_scale
   
   implicit none
   
   character(len=*), intent(in)    :: inLine
-  integer,          intent(inout) :: iElem
+  integer,          intent(inout) :: iLine
   logical,          intent(inout) :: iErr
   
   character(len=:), allocatable   :: lnSplit(:)
@@ -140,11 +140,6 @@ subroutine sixin_parseInputLineSING(inLine, iElem, iErr)
   integer i
   
   call chr_split(inLine, lnSplit, nSplit)
-  
-  ! write(lout,"(a,i0,a)") "SING> Line(",nSplit,"): '"//chr_trimZero(inLine)//"'"
-  ! do i=1,nSplit
-  !   write(lout,"(a,i3,a)") "SING> ",i,": '"//chr_trimZero(lnSplit(i))//"'"
-  ! end do
   
   if(nSplit <= 2) then
     write(lout,"(a,i0)") "GEOMETRY> ERROR Single element line must have more than 2 values, got ",nSplit
@@ -160,7 +155,7 @@ subroutine sixin_parseInputLineSING(inLine, iElem, iErr)
   end if
   
   ! Check that the name is unique
-  do i=1,iElem-1
+  do i=1,sixin_nSing-1
     if(bez(i) == elemName) then
       write(lout,"(a,i0)") "GEOMETRY> ERROR Single element '"//elemName//"' is not unique."
       iErr = .true.
@@ -169,30 +164,30 @@ subroutine sixin_parseInputLineSING(inLine, iElem, iErr)
   end do
   
   ! Save Values
-  if(nSplit > 1) kz(iElem)   = chr_toInt(lnSplit(2))
-  if(nSplit > 2) ed(iElem)   = chr_toReal(lnSplit(3))
-  if(nSplit > 3) ek(iElem)   = chr_toReal(lnSplit(4))
-  if(nSplit > 4) el(iElem)   = chr_toReal(lnSplit(5))
-  if(nSplit > 5) bbbx(iElem) = chr_toReal(lnSplit(6))
-  if(nSplit > 6) bbby(iElem) = chr_toReal(lnSplit(7))
-  if(nSplit > 7) bbbs(iElem) = chr_toReal(lnSplit(8))
+  if(nSplit > 1) kz(sixin_nSing)   = chr_toInt(lnSplit(2))
+  if(nSplit > 2) ed(sixin_nSing)   = chr_toReal(lnSplit(3))
+  if(nSplit > 3) ek(sixin_nSing)   = chr_toReal(lnSplit(4))
+  if(nSplit > 4) el(sixin_nSing)   = chr_toReal(lnSplit(5))
+  if(nSplit > 5) bbbx(sixin_nSing) = chr_toReal(lnSplit(6))
+  if(nSplit > 6) bbby(sixin_nSing) = chr_toReal(lnSplit(7))
+  if(nSplit > 7) bbbs(sixin_nSing) = chr_toReal(lnSplit(8))
   
-  if(kz(iElem) == 25) then
-    ed(iElem) = ed(iElem)/two
-    ek(iElem) = ek(iElem)/two
+  if(kz(sixin_nSing) == 25) then
+    ed(sixin_nSing) = ed(sixin_nSing)/two
+    ek(sixin_nSing) = ek(sixin_nSing)/two
   endif
   
   ! CHANGING SIGN OF CURVATURE OF VERTICAL THICK DIPOLE
-  if((kz(iElem) == 4 .or. kz(iElem) == 5) .and. abs(el(iElem)) > pieni) then
-    ed(iElem) = -one*ed(iElem)
+  if((kz(sixin_nSing) == 4 .or. kz(sixin_nSing) == 5) .and. abs(el(sixin_nSing)) > pieni) then
+    ed(sixin_nSing) = -one*ed(sixin_nSing)
   end if
   
   ! CAVITIES
-  if(abs(kz(iElem)) == 12) then
-    if(abs(ed(iElem)) > pieni .and. abs(ek(iElem)) > pieni) then
+  if(abs(kz(sixin_nSing)) == 12) then
+    if(abs(ed(sixin_nSing)) > pieni .and. abs(ek(sixin_nSing)) > pieni) then
       sixin_ncy2    = sixin_ncy2 + 1
-      itionc(iElem) = kz(iElem)/abs(kz(iElem))
-      kp(iElem)     = 6
+      itionc(sixin_nSing) = kz(sixin_nSing)/abs(kz(sixin_nSing))
+      kp(sixin_nSing)     = 6
     end if
   end if
   
@@ -205,39 +200,39 @@ subroutine sixin_parseInputLineSING(inLine, iElem, iErr)
   ! CAVITY (+/- 12)
   ! CRABCAVITY (23/-23) / CC multipoles order 2/3/4 (+/- 23/26/27/28)
   ! ELECTRON LENSE (29)
-  call initialize_element(iElem,.true.)
+  call initialize_element(sixin_nSing,.true.)
   
   ! ACDIPOLE
-  if(abs(kz(iElem)) == 16) then
-    if(abs(ed(iElem)) <= pieni) then
-      kz(iElem) = 0
-      ed(iElem) = zero
-      ek(iElem) = zero
-      el(iElem) = zero
+  if(abs(kz(sixin_nSing)) == 16) then
+    if(abs(ed(sixin_nSing)) <= pieni) then
+      kz(sixin_nSing) = 0
+      ed(sixin_nSing) = zero
+      ek(sixin_nSing) = zero
+      el(sixin_nSing) = zero
     else
-      acdipph(iElem) = el(iElem)
-      el(iElem)      = zero
+      acdipph(sixin_nSing) = el(sixin_nSing)
+      el(sixin_nSing)      = zero
     end if
   end if
   
   ! General
-  if(abs(el(iElem)) > pieni .and. kz(iElem) /= 0) then
+  if(abs(el(sixin_nSing)) > pieni .and. kz(sixin_nSing) /= 0) then
     ithick = 1
   end if
   
   ! Expand Arrays
-  if(iElem > nele-2) then
+  if(sixin_nSing > nele-2) then
     call expand_arrays(nele+50, npart, nblz, nblo)
     ! The value of nele will have been updated here
     call resize(sixin_bez0, str_maxName, nele, str_nmZeros, "sixin_bez0")
   end if
   
-  if(abs(kz(iElem)) /= 12 .or. (abs(kz(iElem)) == 12 .and. sixin_ncy2 == 0)) then
-    kp(iElem) = 0
+  if(abs(kz(sixin_nSing)) /= 12 .or. (abs(kz(sixin_nSing)) == 12 .and. sixin_ncy2 == 0)) then
+    kp(sixin_nSing) = 0
   end if
   
-  bez(iElem)        = elemName
-  sixin_bez0(iElem) = elemName
+  bez(sixin_nSing)        = elemName
+  sixin_bez0(sixin_nSing) = elemName
   
   !If no active RF cavities are seen so far in the single element list,
   ! add a CAV element to the end of the list.
@@ -246,14 +241,14 @@ subroutine sixin_parseInputLineSING(inLine, iElem, iErr)
   ! used in the structure to enable 6D tracking using the parameters
   ! from the SYNC block.
   if(sixin_ncy2 == 0) then
-    iElem = iElem + 1
-    il    = iElem
-    bez(iElem)        = sixin_cavity
-    sixin_bez0(iElem) = sixin_cavity
-    kp(iElem)         = 6
+    sixin_nSing             = sixin_nSing + 1
+    il                      = sixin_nSing
+    bez(sixin_nSing)        = sixin_cavity
+    sixin_bez0(sixin_nSing) = sixin_cavity
+    kp(sixin_nSing)         = 6
   else
-    il    = iElem
-    iElem = iElem + 1
+    il          = sixin_nSing
+    sixin_nSing = sixin_nSing + 1
   end if
   
 end subroutine sixin_parseInputLineSING
