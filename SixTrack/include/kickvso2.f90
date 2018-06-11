59a95c81eb6ccdc66178e09f638f914cabfebb78
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-06-11 19:48:35 +0200

    A first step to implement the solenoid correctly.

--- SixTrack/include/kickvso2.f90
+++ SixTrack/include/kickvso2.f90
@@ -1,5 +1,5 @@
-crkve=sigmv(j)-half*(((((((xv(1,j)**2+xv(2,j)**2)*strackx(i))*strackz(i))*rvv(j))*ejf0v(j))/ejfv(j))*ejf0v(j))/ejfv(j)
-sigmv(j)=crkve
-crkve=yv(1,j)-(((xv(1,j)*strackx(i))*strackz(i))*ejf0v(j))/ejfv(j)
-cikve=yv(2,j)-(((xv(2,j)*strackx(i))*strackz(i))*ejf0v(j))/ejfv(j)
-sigmv(j)=sigmv(j)+((((((xv(1,j)*cikve-xv(2,j)*crkve)*strackz(i))*rvv(j))*ejf0v(j))/ejfv(j))*ejf0v(j))/ejfv(j)
+!crkve=sigmv(j)-half*(((((((xv(1,j)**2+xv(2,j)**2)*strackx(i))*strackz(i))*rvv(j))*ejf0v(j))/ejfv(j))*ejf0v(j))/ejfv(j)
+!sigmv(j)=crkve
+!crkve=yv(1,j)-(((xv(1,j)*strackx(i))*strackz(i))*ejf0v(j))/ejfv(j)
+!cikve=yv(2,j)-(((xv(2,j)*strackx(i))*strackz(i))*ejf0v(j))/ejfv(j)
+!sigmv(j)=sigmv(j)+((((((xv(1,j)*cikve-xv(2,j)*crkve)*strackz(i))*rvv(j))*ejf0v(j))/ejfv(j))*ejf0v(j))/ejfv(j)
