82759286d92e4df05bcf3e3a35db0de0bbde9335
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-06-13 11:41:21 +0200

    Implementation works and gives consistent results between track and FOX.

--- SixTrack/include/kickfso1.f90
+++ SixTrack/include/kickfso1.f90
@@ -1,12 +1,49 @@
-!FOX  Y(1)=Y(1)-X(2)*ED(IX) ;
-!FOX  Y(2)=Y(2)+X(1)*ED(IX) ;
-!FOX  CRKVE=Y(1)-X(1)*ED(IX)*EK(IX) ;
-!FOX  CIKVE=Y(2)-X(2)*ED(IX)*EK(IX) ;
-!FOX  Y(1)=CRKVE*COS(EK(IX))+CIKVE*SIN(EK(IX)) ;
-!FOX  Y(2)=-CRKVE*SIN(EK(IX))+CIKVE*COS(EK(IX)) ;
-!FOX  CRKVE=X(1)*COS(EK(IX))+X(2)*SIN(EK(IX)) ;
-!FOX  CIKVE=-X(1)*SIN(EK(IX))+X(2)*COS(EK(IX)) ;
-!FOX  X(1)=CRKVE ;
-!FOX  X(2)=CIKVE ;
-!FOX  Y(1)=Y(1)+X(2)*ED(IX) ;
-!FOX  Y(2)=Y(2)-X(1)*ED(IX) ;
+!strack=zero
+!strackx=ed(IX)
+!strackz=ek(IX)
+
+!Going to momenum and psigma
+
+!FOX  YP(1)=Y(1)*(ONE+DPDA)/MTCDA ;
+!FOX  YP(2)=Y(2)*(ONE+DPDA)/MTCDA ;
+!FOX  PUSIG=((EJ1-E0)/(E0F*(E0F/E0)))*C1E3 ;
+
+!FOX  TEMPI(1) = X(1) ;
+!FOX  TEMPI(2) = YP(1) ;
+!FOX  TEMPI(3) = X(2) ;
+!FOX  TEMPI(4) = YP(2) ;
+!FOX  TEMPI(5) = SIGMDA ;
+!FOX  TEMPI(6) = PUSIG ;
+
+
+!ONEDPDA,FPSIGDA,FPPSIGDA,COSTH_DA,SINTH_DA,Q_DA,R_DA,Z_DA,PXFDA,PYFDA,SIGFDA
+
+!Full 6d-formula using 
+
+!FOX  ONEDPDA   = SQRT(ONE + TWO*TEMPI(6)+((E0F/E0)*(E0F/E0))*(TEMPI(6)*TEMPI(6))) ;
+!FOX  FPPSIGDA  = ( ONE + ((E0F/E0)*(E0F/E0))*TEMPI(6) ) / ONEDPDA ;
+
+!     Set up C,S, Q_DA,R_DA,Z
+!FOX  COSTH_DA = COS(EK(IX)/ONEDPDA) ;
+!FOX  SINTH_DA = SIN(EK(IX)/ONEDPDA) ;
+
+!FOX  Q_DA = -EK(IX) * ED(IX) / ONEDPDA ;
+!FOX  R_DA = FPPSIGDA / (ONEDPDA*ONEDPDA) * EK(IX) * ED(IX) ;
+!FOX  Z_DA = FPPSIGDA / (ONEDPDA*ONEDPDA) * EK(IX) ;
+
+!FOX  PXFDA  = TEMPI(2) + TEMPI(1)*Q_DA ;
+!FOX  PYFDA  = TEMPI(4) +  TEMPI(3) *Q_DA ;
+!FOX  SIGFDA = TEMPI(5) - (ONE/TWO)*(TEMPI(1)*TEMPI(1) +  TEMPI(3) *TEMPI(3))*R_DA ;
+
+!       R_DAipken formulae p.29 (3.37)
+!FOX  X(1) =  (TEMPI(1)  * COSTH_DA  +  TEMPI(3)  * SINTH_DA) ;
+!FOX  YP(1) =  (PXFDA * COSTH_DA  +  PYFDA * SINTH_DA) ;
+!FOX  X(2) = (-TEMPI(1)  * SINTH_DA  +  TEMPI(3)  * COSTH_DA) ;
+!FOX  YP(2) = (-PXFDA * SINTH_DA  +  PYFDA * COSTH_DA) ;
+!FOX  SIGMDA =  SIGFDA + (TEMPI(1)*PYFDA - TEMPI(3)*PXFDA)*Z_DA ;
+
+print *, "ek",  ek(ix)
+print *, "ed",  ed(ix)
+!Going back to angles
+!FOX  Y(1)=YP(1)*MTCDA/(ONE+DPDA) ; 
+!FOX  Y(2)=YP(2)*MTCDA/(ONE+DPDA) ;
--- SixTrack/include/kickvso1.f90
+++ SixTrack/include/kickvso1.f90
@@ -1,4 +1,4 @@
-print *,"strackxxxxxx", strackx(i) !This i ks/2 
+!!print *,"strackxxxxxx", strackx(i) !This i ks/2 
 print *,"strackzzzzz", strackz(i)  !ksi/2
 
 ! x'-> px; y'->py
@@ -14,8 +14,7 @@ temptr(6)=((mtc(j)*ejv(j)-e0)/e0f)*c1e3*(e0/e0f)
 
 
 !     We do not use a constant deltap!!!!! WE use full 6D formulae!
-onedp   = sqrt( one + two*temptr(6) + ((e0f/e0)**2)*(temptr(6)**2) )
-fpsig   = onedp - one
+onedp   = sqrt( one + two*temptr(6) + ((e0f/e0)**2)*(temptr(6)**2))
 fppsig  = ( one + ((e0f/e0) **2)*temptr(6) ) / onedp
 
 !     Set up C,S, q_temp,r_temp,Z
@@ -27,38 +26,18 @@ z_temp = fppsig / (onedp**2) * strackz(i)
 
 pxf  = temptr(2) + temptr(1)*q_temp
 pyf  = temptr(4) +  temptr(3) *q_temp
-sigf = sigmv(j)*(e0f/e0) - half*(temptr(1)**2 +  temptr(3) **2)*r_temp
+sigf = sigmv(j) - half*(temptr(1)**2 +  temptr(3) **2)*r_temp
 
+print *, "ek", strackz(i), ek(i)
+print *, "ed", strackx(i), ed(i)
 !       r_tempipken formulae p.29 (3.37)
 xv(1,j) =  temptr(1)  * costh_temp  +  temptr(3)  * sinth_temp
 yv(1,j) =  pxf * costh_temp  +  pyf * sinth_temp
 xv(2,j) = -temptr(1)  * sinth_temp  +  temptr(3)  * costh_temp
 yv(2,j) = -pxf * sinth_temp  +  pyf * costh_temp
-sigmv(j) =  (sigf + (temptr(1)*pyf - temptr(3)*pxf)*z_temp)
+sigmv(j) =  sigf + (temptr(1)*pyf - temptr(3)*pxf)*z_temp
 
 
-yv(j,1) = yv(j,1)*mtc(j)/(one+dpsv(j))
-yv(j,2) = yv(j,2)*mtc(j)/(one+dpsv(j))
+yv(j,1) = yv(j,1)*moidpsv(j)
+yv(j,2) = yv(j,2)*moidpsv(j)
 
-
-
-
-
-
-
-
-!yv(1,j)=yv(1,j)-xv(2,j)*strackx(i)
-!yv(2,j)=yv(2,j)+xv(1,j)*strackx(i)
-!
-! TODO: Check if ejf0v should be e0f?? or oidpsv=ejf0v(j)/ejfv(j)=1/(1+delta)
-!crkve, cikve
-!crkve=yv(1,j)-(((xv(1,j)*strackx(i))*strackz(i))*ejf0v(j))/ejfv(j) !hr02
-!cikve=yv(2,j)-(((xv(2,j)*strackx(i))*strackz(i))*ejf0v(j))/ejfv(j) !hr02
-!yv(1,j)=crkve*cos_mb((strackz(i)*ejf0v(j))/ejfv(j))+cikve*sin_mb((strackz(i)*ejf0v(j))/ejfv(j))
-!yv(2,j)=cikve*cos_mb((strackz(i)*ejf0v(j))/ejfv(j))-crkve*sin_mb((strackz(i)*ejf0v(j))/ejfv(j))
-!crkve=xv(1,j)*cos_mb((strackz(i)*ejf0v(j))/ejfv(j))+xv(2,j)*sin_mb((strackz(i)*ejf0v(j))/ejfv(j))
-!cikve=xv(2,j)*cos_mb((strackz(i)*ejf0v(j))/ejfv(j))-xv(1,j)*sin_mb((strackz(i)*ejf0v(j))/ejfv(j))
-!xv(1,j)=crkve
-!xv(2,j)=cikve
-!yv(1,j)=yv(1,j)+xv(2,j)*strackx(i)
-!yv(2,j)=yv(2,j)-xv(1,j)*strackx(i)
