8f677c188449de4e767f26ef0df3b48a40b01e50
Tobias <tpersson@cern.ch>
2018-06-14 15:03:36 +0200

    There was a missing 10-3 for the update of sigma.

--- SixTrack/include/kickfso1.f90
+++ SixTrack/include/kickfso1.f90
@@ -6,19 +6,17 @@
 
 !FOX  YP(1)=Y(1)*(ONE+DPDA)/MTCDA ;
 !FOX  YP(2)=Y(2)*(ONE+DPDA)/MTCDA ;
-!FOX  PUSIG=((EJ1-E0)/(E0F*(E0F/E0)))*C1E3 ;
+!FOX  PUSIG=((EJ1-E0)/(E0F*(E0F/E0))) ;
 
-!FOX  TEMPI(1) = X(1) ;
-!FOX  TEMPI(2) = YP(1) ;
-!FOX  TEMPI(3) = X(2) ;
-!FOX  TEMPI(4) = YP(2) ;
-!FOX  TEMPI(5) = SIGMDA ;
+!FOX  TEMPI(1) = X(1)*C1M3 ;
+!FOX  TEMPI(2) = YP(1)*C1M3 ;
+!FOX  TEMPI(3) = X(2)*C1M3 ;
+!FOX  TEMPI(4) = YP(2)*C1M3 ;
+!FOX  TEMPI(5) = SIGMDA*C1M3 ;
 !FOX  TEMPI(6) = PUSIG ;
 
 
-!ONEDPDA,FPSIGDA,FPPSIGDA,COSTH_DA,SINTH_DA,Q_DA,R_DA,Z_DA,PXFDA,PYFDA,SIGFDA
-
-!Full 6d-formula using 
+!Full 6d-formula 
 
 !FOX  ONEDPDA   = SQRT(ONE + TWO*TEMPI(6)+((E0F/E0)*(E0F/E0))*(TEMPI(6)*TEMPI(6))) ;
 !FOX  FPPSIGDA  = ( ONE + ((E0F/E0)*(E0F/E0))*TEMPI(6) ) / ONEDPDA ;
@@ -36,14 +34,12 @@
 !FOX  SIGFDA = TEMPI(5) - (ONE/TWO)*(TEMPI(1)*TEMPI(1) +  TEMPI(3) *TEMPI(3))*R_DA ;
 
 !       R_DAipken formulae p.29 (3.37)
-!FOX  X(1) =  (TEMPI(1)  * COSTH_DA  +  TEMPI(3)  * SINTH_DA) ;
-!FOX  YP(1) =  (PXFDA * COSTH_DA  +  PYFDA * SINTH_DA) ;
-!FOX  X(2) = (-TEMPI(1)  * SINTH_DA  +  TEMPI(3)  * COSTH_DA) ;
-!FOX  YP(2) = (-PXFDA * SINTH_DA  +  PYFDA * COSTH_DA) ;
-!FOX  SIGMDA =  SIGFDA + (TEMPI(1)*PYFDA - TEMPI(3)*PXFDA)*Z_DA ;
+!FOX  X(1) =  (TEMPI(1)  * COSTH_DA  +  TEMPI(3)  * SINTH_DA)*C1E3 ;
+!FOX  YP(1) =  (PXFDA * COSTH_DA  +  PYFDA * SINTH_DA)*C1E3 ;
+!FOX  X(2) = (-TEMPI(1)  * SINTH_DA  +  TEMPI(3)  * COSTH_DA)*C1E3 ;
+!FOX  YP(2) = (-PXFDA * SINTH_DA  +  PYFDA * COSTH_DA)*C1E3 ;
+!FOX  SIGMDA =  (SIGFDA + (TEMPI(1)*PYFDA - TEMPI(3)*PXFDA)*Z_DA)*C1E3 ;
 
-print *, "ek",  ek(ix)
-print *, "ed",  ed(ix)
 !Going back to angles
 !FOX  Y(1)=YP(1)*MTCDA/(ONE+DPDA) ; 
 !FOX  Y(2)=YP(2)*MTCDA/(ONE+DPDA) ;
--- SixTrack/include/kickvso1.f90
+++ SixTrack/include/kickvso1.f90
@@ -1,19 +1,16 @@
 !!print *,"strackxxxxxx", strackx(i) !This i ks/2 
-print *,"strackzzzzz", strackz(i)  !ksi/2
+!!print *,"strackzzzzz", strackz(i)  !ksi/2
 
 ! x'-> px; y'->py
 !Transformation to some more reasonable coordinates
-temptr(1)=xv(1,j)
-temptr(2)=yv(1,j)/moidpsv(j)
-temptr(3)=xv(2,j)
-temptr(4)=yv(2,j)/moidpsv(j)
+temptr(1)=xv(1,j)*c1m3
+temptr(2)=(yv(1,j)*c1m3)/moidpsv(j)
+temptr(3)=xv(2,j)*c1m3
+temptr(4)=(yv(2,j)*c1m3)/moidpsv(j)
 !temptr(5)=sigmv(j)
-temptr(6)=((mtc(j)*ejv(j)-e0)/e0f)*c1e3*(e0/e0f) 
+temptr(6)=(ejv(j)-e0)/(e0f*(e0f/e0))
 
 
-
-
-!     We do not use a constant deltap!!!!! WE use full 6D formulae!
 onedp   = sqrt( one + two*temptr(6) + ((e0f/e0)**2)*(temptr(6)**2))
 fppsig  = ( one + ((e0f/e0) **2)*temptr(6) ) / onedp
 
@@ -26,17 +23,15 @@ z_temp = fppsig / (onedp**2) * strackz(i)
 
 pxf  = temptr(2) + temptr(1)*q_temp
 pyf  = temptr(4) +  temptr(3) *q_temp
-sigf = sigmv(j) - half*(temptr(1)**2 +  temptr(3) **2)*r_temp
+sigf = sigmv(j)*c1m3 - half*(temptr(1)**2 +  temptr(3) **2)*r_temp
 
-print *, "ek", strackz(i), ek(i)
-print *, "ed", strackx(i), ed(i)
+
 !       r_tempipken formulae p.29 (3.37)
-xv(1,j) =  temptr(1)  * costh_temp  +  temptr(3)  * sinth_temp
-yv(1,j) =  pxf * costh_temp  +  pyf * sinth_temp
-xv(2,j) = -temptr(1)  * sinth_temp  +  temptr(3)  * costh_temp
-yv(2,j) = -pxf * sinth_temp  +  pyf * costh_temp
-sigmv(j) =  sigf + (temptr(1)*pyf - temptr(3)*pxf)*z_temp
-
+xv(1,j) =  (temptr(1)  * costh_temp  +  temptr(3)  * sinth_temp)*c1e3
+yv(1,j) =  (pxf * costh_temp  +  pyf * sinth_temp)*c1e3
+xv(2,j) = (-temptr(1)  * sinth_temp  +  temptr(3)  * costh_temp)*c1e3
+yv(2,j) = (-pxf * sinth_temp  +  pyf * costh_temp)*c1e3
+sigmv(j) =  (sigf + (temptr(1)*pyf - temptr(3)*pxf)*z_temp)*c1e3
 
 yv(j,1) = yv(j,1)*moidpsv(j)
 yv(j,2) = yv(j,2)*moidpsv(j)
