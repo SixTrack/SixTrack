5d9357916821f31f391bd77be134cd7204012ae7
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-06-15 00:23:48 +0200

    Commenting out old variables in debug_dump

--- source/debug_dump.f90
+++ source/debug_dump.f90
@@ -302,7 +302,7 @@ subroutine dump(dumpname,n,i)
   write(99,*) 'izu0 ',izu0
   write(99,*) 'mmac ',mmac
   write(99,*) 'mcut ',mcut
-  write(99,*) 'exterr ',exterr
+! write(99,*) 'exterr ',exterr
   write(99,*) 'extalign ',extalign
   write(99,*) 'tiltc ',tiltc
   write(99,*) 'tilts ',tilts
@@ -801,7 +801,7 @@ subroutine dumpbin(dumpname,n,i)
   write(99) izu0
   write(99) mmac
   write(99) mcut
-  write(99) exterr
+! write(99) exterr
   write(99) extalign
   write(99) tiltc
   write(99) tilts
@@ -1294,7 +1294,7 @@ subroutine dumphex(dumpname,n,i)
   write(99,100) 'izu0 ',izu0
   write(99,100) 'mmac ',mmac
   write(99,100) 'mcut ',mcut
-  write(99,100) 'exterr ',exterr
+! write(99,100) 'exterr ',exterr
   write(99,100) 'extalign ',extalign
   write(99,100) 'tiltc ',tiltc
   write(99,100) 'tilts ',tilts
