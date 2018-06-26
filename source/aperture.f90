534745a5a75be6ab2edc5e777ea114422bb20447
James Molson <james.molson@cern.ch>
2018-06-26 11:11:01 +0200

    Minor change in the aperture backtracking.

--- source/aperture.f90
+++ source/aperture.f90
@@ -923,9 +923,9 @@ subroutine lostpart(turn, i, ix, llost, nthinerr)
             select case(iBckTypeLast)
             case (0)
               ! back-track along a drift
-              xlos(1) = xLast(1,j)  - yLast(1,j)*(one-step)*length
-              xlos(2) = xLast(2,j)  - yLast(2,j)*(one-step)*length
-              slos    = dcum(iLast) - (one-step)*length
+              xlos(1) = xLast(1,j)  - yLast(1,j)*((one-step)*length)
+              xlos(2) = xLast(2,j)  - yLast(2,j)*((one-step)*length)
+              slos    = dcum(iLast) - (step*length)
             end select
 
             ! - aperture at current step
