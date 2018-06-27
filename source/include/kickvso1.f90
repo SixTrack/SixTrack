f68ec83c2f0e9ac6872c51c958c19f8aee4850a9
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-06-27 11:06:49 +0200

    Now the difference is very small for the test. Still might have an impact on the closed orbit it looks like.

--- source/include/kickvso1.f90
+++ source/include/kickvso1.f90
@@ -1,16 +1,19 @@
-!!print *,"strackxxxxxx", strackx(i) !This i ks/2 
-!!print *,"strackzzzzz", strackz(i)  !ksi/2
+!print *,"strackxxxxxx", strackx(i) !This i ks/2 
+!print *,"strackzzzzz", strackz(i)  !ksi/2
 
 ! x'-> px; y'->py
 !Transformation to some more reasonable coordinates
-temptr(1)=xv(1,j)*c1m3
-temptr(2)=(yv(1,j)*c1m3)/moidpsv(j)
-temptr(3)=xv(2,j)*c1m3
-temptr(4)=(yv(2,j)*c1m3)/moidpsv(j)
+
+temptr(1)=xv(1,j)
+temptr(2)=yv(1,j)/moidpsv(j)
+temptr(3)=xv(2,j)
+temptr(4)=yv(2,j)/moidpsv(j)
 
 temptr(6)=(ejv(j)-e0)/(e0f*(e0f/e0))
 
-
+!yv(1,j) = temptr(2)
+!yv(2,j) = temptr(4)
+!print *, "before", xv(1,j), yv(1,j), xv(2,j), yv(2,j), sigmv(j), moidpsv(j), j
 onedp   = sqrt( one + two*temptr(6) + ((e0f/e0)**2)*(temptr(6)**2))
 fppsig  = ( one + ((e0f/e0) **2)*temptr(6) ) / onedp
 
@@ -18,20 +21,24 @@ fppsig  = ( one + ((e0f/e0) **2)*temptr(6) ) / onedp
 costh_temp = cos_mb(strackz(i)/onedp)
 sinth_temp = sin_mb(strackz(i)/onedp)
 q_temp = -strackz(i) * strackx(i) / onedp
-r_temp = fppsig / (onedp**2) * strackz(i) * strackx(i)
-z_temp = fppsig / (onedp**2) * strackz(i)
+r_temp = (fppsig / (onedp**2)) * (strackz(i) * strackx(i))
+z_temp = (fppsig / (onedp**2))* (strackz(i))
 
+
+!print *,"costh_temp", costh_temp !This i ks/2 
+!print *,"sinth_temp", sinth_temp  !ksi/2
 pxf  = temptr(2) + temptr(1)*q_temp
 pyf  = temptr(4) +  temptr(3) *q_temp
 
+!print *, "pxf", pxf , temptr(2)
+!print *, "pyf", pyf , temptr(4)
 
 !       r_tempipken formulae p.29 (3.37)
-xv(1,j) =  (temptr(1)  * costh_temp  +  temptr(3)  * sinth_temp)*c1e3
-yv(1,j) =  (pxf * costh_temp  +  pyf * sinth_temp)*c1e3
-xv(2,j) = (-temptr(1)  * sinth_temp  +  temptr(3)  * costh_temp)*c1e3
-yv(2,j) = (-pxf * sinth_temp  +  pyf * costh_temp)*c1e3
+xv(1,j) =  (temptr(1)  * costh_temp  +  temptr(3)  * sinth_temp)
+yv(1,j) =  (pxf * costh_temp  +  pyf * sinth_temp)*moidpsv(j)
+xv(2,j) = (-temptr(1)  * sinth_temp  +  temptr(3)  * costh_temp)
+yv(2,j) = (-pxf * sinth_temp  +  pyf * costh_temp)*moidpsv(j)
 
 
-yv(j,1) = yv(j,1)*moidpsv(j)
-yv(j,2) = yv(j,2)*moidpsv(j)
 
+!print *, "after", xv(1,j), yv(1,j), xv(2,j), yv(2,j), sigmv(j), moidpsv(j), j
--- source/include/kickvso2.f90
+++ source/include/kickvso2.f90
@@ -1,2 +1,5 @@
-sigf = sigmv(j)*c1m3 - half*(temptr(1)**2 +  temptr(3) **2)*r_temp
-sigmv(j) =  (sigf + (temptr(1)*pyf - temptr(3)*pxf)*z_temp)*c1e3
+print *, sigmv(j), j
+sigf = sigmv(j) - (half*(temptr(1)**2 +  temptr(3) **2)*r_temp)*c1m3
+sigmv(j) =  (sigf + (temptr(1)*pyf - temptr(3)*pxf)*(z_temp*c1m3))
+!print *, xv(1,j), yv(1,j), xv(2,j), yv(2,j), sigmv(j)
+print *, sigmv(j), j
