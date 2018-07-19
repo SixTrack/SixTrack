e515239fb83eb7642b35e4c352924cf8121ca0c6
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-07-19 11:51:45 +0200

    Horizontal combined function implemented.

--- source/include/mul4v05.f90
+++ source/include/mul4v05.f90
@@ -1,4 +1,9 @@
-yv1j=(bbiv(1,1,i)+bbiv(2,1,i)*xlvj)+aaiv(2,1,i)*zlvj       !hr03
-yv2j=(aaiv(1,1,i)-bbiv(2,1,i)*zlvj)+aaiv(2,1,i)*xlvj       !hr03
+yv1j=bbiv(1,1,i)+bbiv(2,1,i)*xlvj+aaiv(2,1,i)*zlvj + &
+bbiv(2,1,i)*((strack(i)*(xlvj**2-0.5*zlvj**2)*c1m3))   
+
+yv2j=(aaiv(1,1,i)-bbiv(2,1,i)*zlvj)+aaiv(2,1,i)*xlvj- &
+bbiv(2,1,i)*((strack(i))*((xlvj*zlvj*c1m3)+((dki(i,1)*c1m6*zlvj**3)/6.0)))    
+
+
 crkve=xlvj
 cikve=zlvj
--- source/include/multf02.f90
+++ source/include/multf02.f90
@@ -1,4 +1,10 @@
-!FOX  YV1J=BBI(I,1)+BBI(I,2)*XL+AAI(I,2)*ZL ;
-!FOX  YV2J=AAI(I,1)-BBI(I,2)*ZL+AAI(I,2)*XL ;
+!Here it is in angles because the transformation is afterwards
+
+!FOX  YV1J=BBI(I,1)+BBI(I,2)*XL+AAI(I,2)*ZL + 
+!FOX  BBI(I,2)*((DKI(I,1)/DKI(I,3))*(XL*XL-0.5*ZL*ZL)*C1M3) ; 
+
+!FOX  YV2J=AAI(I,1)-BBI(I,2)*ZL+AAI(I,2)*XL-
+!FOX  BBI(I,2)*(DKI(I,1)/DKI(I,3))*(XL*ZL*C1M3+C1M6*(DKI(I,1))*(ZL*ZL*ZL/(6.0)));
+
 !FOX  CRKVE=XL ;
 !FOX  CIKVE=ZL ;
