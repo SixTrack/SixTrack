a3bd71733bcd959c010e3cc5c0a84bc83d3c6f48
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-07-30 18:13:35 +0200

    Added pytjia build script and pythia wrapper source files, and updated CMakeLists.

---
+++ source/pythia/pythia_wrapper.cpp
@@ -0,0 +1,8 @@
+/**
+ *  SixTrack Wrapper for Pythia8
+ *  V.K. Berglyd Olsen, BE-ABP-HSS
+ *  Last modified: 2018-07-30
+**/
+
+#include "pythia_wrapper.hpp"
+
---
+++ source/pythia/pythia_wrapper.hpp
@@ -0,0 +1,8 @@
+/**
+ *  SixTrack Wrapper for Pythia8
+ *  V.K. Berglyd Olsen, BE-ABP-HSS
+ *  Last modified: 2018-07-30
+**/
+
+#include "Pythia8/Pythia.h"
+
