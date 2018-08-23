cd30063743d49f1aa106be68cb7650530fca7e40
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-08-23 13:02:53 +0200

    Made libraries in buildLibraries selectable and added hdf5 build script (incomplete).

--- buildLibraries.sh
+++ buildLibraries.sh
@@ -8,16 +8,46 @@ echo " Building SixTrack Library Dependecies"
 echo "========================================"
 echo ""
 
-# Make sure we have the right submodule versions
-echo "Updating submodules ..."
-git submodule init
-git submodule update
-echo "Done"
+ALL=true
+BOINC=false
+LIBARCH=false
+HDF5=false
 
-cd lib
+for ARG in "$@"; do
+    if [[ $ARG == "boinc" ]]; then
+        BOINC=true
+        LIBARCH=true
+        echo "Boinc depends on libarchive, libarchive enabled as well."
+    elif [[ $ARG == "libarchive" ]]; then
+        LIBARCH=true
+    elif [[ $ARG == "hdf5" ]]; then
+        HDF5=true
+    else
+        echo "Unknown library $ARG requested."
+        exit 1
+    fi
+    echo "Will build $ARG"
+    ALL=false
+done
 
-# Run individual build scripts
-./buildBoinc.sh
-./buildLibarchive.sh
+if [ $BOINC = true ] || [ $ALL = true ]; then
+    git submodule init lib/boinc
+    git submodule update lib/boinc
+    cd lib
+    ./buildBoinc.sh
+    cd ..
+fi
 
-cd ..
+if [ $LIBARCH = true ] || [ $ALL = true ]; then
+    git submodule init lib/libarchive
+    git submodule update lib/libarchive
+    cd lib
+    ./buildLibarchive.sh
+    cd ..
+fi
+
+if [ $HDF5 = true ] || [ $ALL = true ]; then
+    cd lib
+    ./buildHDF5.sh
+    cd ..
+fi
---
+++ lib/buildHDF5.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e #Exit on error
+
+echo ""
+echo " Building HDF5"
+echo "==============="
+echo ""
+
+VERSION=1.10
+PATCH=1.10.1
+
+mkdir -pv hdf5
+cd hdf5
+
+# Download links from https://support.hdfgroup.org/HDF5/release/cmakebuild.html
+if [[ $(uname) == MINGW* ]]; then
+  if [ ! -f CMake-hdf5-1.10.1.zip ]; then
+    SOURCE=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$VERSION/hdf5-$PATCH/src/CMake-hdf5-$PATCH.zip
+    wget $SOURCE
+    unzip CMake-hdf5-$PATCH.zip
+  fi
+else
+  if [ ! -f CMake-hdf5-1.10.1.tar.gz ]; then
+    SOURCE=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$VERSION/hdf5-$PATCH/src/CMake-hdf5-$PATCH.tar.gz
+    wget $SOURCE
+    tar -xf CMake-hdf5-$PATCH.tar.gz
+  fi
+fi
+
+if [ -d build ]; then
+  rm -rfv build
+fi
+
+mkdir build
+cd build
+# cd CMake-hdf5-$PATCH
+
+cd ..
+cd ..
