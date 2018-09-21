4a0a7eae828213705e3564a83ecef0035cba391b
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-09-21 14:26:12 +0200

    Forgot to update one file.

--- source/pytools/update_tests.py
+++ source/pytools/update_tests.py
@@ -3,9 +3,9 @@ from os.path import isfile, join
 from shutil import copyfile
 
 
-testToUpdateArry = ['beam-HO_6D-newstyle','beam-HO_6D-oldstyle', 'beam-HO_6D-simple-oldstyle', 'beam-HO_6D-simple-newstyle', 'beam-HO_6D-simple-newstyle-orbit',
-'beam-HO_LR-newstyle','beam-HO_LR-oldstyle','beam-HO_LR-ibbc-oldstyle', 'beam-HO_LR-ibbc-newstyle', 'scatter_collimation','beam-HO_6D-simple-newstyle'    ]
-
+#testToUpdateArry = ['beam-HO_6D-newstyle','beam-HO_6D-oldstyle', 'beam-HO_6D-simple-oldstyle', 'beam-HO_6D-simple-newstyle', 'beam-HO_6D-simple-newstyle-orbit',
+#'beam-HO_LR-newstyle','beam-HO_LR-oldstyle','beam-HO_LR-ibbc-oldstyle', 'beam-HO_LR-ibbc-newstyle', 'scatter_collimation','beam-HO_6D-simple-newstyle'    ]
+testToUpdateArry = ['beam-HO_6D-simple-newstyle-coupling']
 singeltr = '../../build/SixTrack_cmakesix_BUILD_TESTING_defaultcompiler_defaultbuildtype/SixTrack_50002_crlibm_rn_Linux_gfortran_static_x86_64_64bit_double'
 no_stf = '../../build/SixTrack_cmakesix_BUILD_TESTING_NOSTF_defaultcompiler_defaultbuildtype/SixTrack_50002_crlibm_rn_nostf_Linux_gfortran_static_x86_64_64bit_double'
 
