5c734b7f54f582143d2f4c27a1c9672c455d0aca
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-11-02 09:21:49 +0100

    Gives coupled coordinates.

--- source/dist/dist2/src/cprintarr.c
+++ source/dist/dist2/src/cprintarr.c
@@ -13,6 +13,7 @@ cprintarr_(float *arr, int *nx, int *ny)
             printf("arr[%d] = %f\n", idx, arr[idx]);
         }
 }
+
 double momentum2energy(double momentum, double mass){
 	return sqrt(pow(momentum,2)+pow(mass,2));
 } 
@@ -32,7 +33,6 @@ six2canonical_(double * coord, double *ref_momentum, double *mass, double *canon
 	 *(canonical+3) = *(coord+3)*(1+deltap);
 	 *(canonical+4) = *(coord+4)/rv;
 	 *(canonical+5) = *(coord+5);
- 
 
 }
 
@@ -49,48 +49,54 @@ canonical2six_(double *canonical, double *ref_momentum, double *mass, double *co
 	 *(coord+4) = *(canonical+4)*rv;
 	 *(coord+5) = *(canonical+5);	 
 }
-void
-action2phys(double *acor, double **tas){
-
-
-}
-
 
 
 /* 
     if mtrx_a is (m x n) and mtrx_b is (n x p), 
     the product is an (m x p) matrix
 */
-
-void mtrx_mult_(int *mp, int *np, int *pp, double mtrx_a[6][6], double mtrx_b[6][6], double result[6][6])
+void mtrx_vector_mult_(int *mp, int *np, int *pp, double mtrx_a[6][6], double mtrx_b[6], double result[6])
 {
 
 	int m = *mp;
 	int n = *np;
 	int p = *pp;
-	printf("m, n, p %d, %d, %d", m, n,p);
-   // double **result = malloc (m * sizeof (*result));
-    // if (!result) throw error
+
 
     register int i=0, j=0, k=0;
     for (i = 0; i < m; i++)
     {
-        /* calloc initializes all to '0' */
-      //  result[i] = calloc (p, sizeof (**result));
-        // if (!result[i]) throw error
-    }
-
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < p; j++)
-        {
             for (k = 0; k < n; k++)
             {
-                result [i][j] += mtrx_a [i][k] * mtrx_b [k][j];
-                printf("results %f, %d, %d, \n", result[i][j], i, j);
+                result [i] += mtrx_a [i][k] * mtrx_b [k];
+                
             }
-        }
+        
     }
 
-    
 }
+/*
+void action2sixcoord_(double *e1, double *a1, double *e2, double *a2, double *e3, double *a3, double tas[6][6], double results [6]){
+	a2c_(e1, a1, e2, a2, e3, a3,tas, results);
+	canonical2six_(results, ref_momentum, mass, double canonical)
+double 
+}
+*/
+
+void a2c_(double *e1, double *a1, double *e2, double *a2, double *e3, double *a3, double tas[6][6], double results [6]){
+	double acoord[6];
+	
+	acoord[0]=sqrt(*e1)*cos(*a1);
+	acoord[1]=-sqrt(*e1)*sin(*a1);
+	acoord[2]=sqrt(*e2)*cos(*a2);
+	acoord[3]=-sqrt(*e2)*sin(*a2);
+	acoord[4]=sqrt(*e3)*cos(*a3);
+	acoord[5]=-sqrt(*e3/1000)*sin(*a3);
+
+	int dim, one;
+	dim = 6;
+	one =1;
+	mtrx_vector_mult_(&dim,&dim,&dim, tas, acoord,results);
+
+} 
+
--- source/dist/dist2/src/slice.f90
+++ source/dist/dist2/src/slice.f90
@@ -27,9 +27,8 @@ subroutine readtas
       ierro=0
       ia=0
 
-      !OPEN(nfile, action='read',iostat=ierro, file="fort.90", form='unknown', access='unknown',position='asis')
-       
-      read(nfile,end=510, iostat=ierro) &
+      OPEN(nfile, action='read',iostat=ierro, file="fort.90.canonical", STATUS='old', FORM="UNFORMATTED")
+      read(nfile, iostat=ierro) &
      &     sixtit,commen,cdate,ctime,progrm, &
      &     ifipa,ilapa,itopa,icode,numl, &
      &     qwc_tmp(1),qwc_tmp(2),qwc_tmp(3), &
@@ -55,9 +54,25 @@ subroutine readtas
      print *, ierro, icode, numl
      print *, ta_tmp
 
-510 continue 
-  print *, "errrorrr"
+
      end subroutine readtas 
+
+
+subroutine readMatrixFromFile(matrix)
+   ! opening the file for reading
+         use, intrinsic :: iso_fortran_env
+    real(kind=real64), dimension(36) :: reada
+    real(kind=real64), dimension(6, 6) :: matrix
+   open (2, file = 'data1.dat', status = 'old')
+
+    
+      read(2,*) reada
+      matrix=RESHAPE(reada, shape(matrix)  )
+   
+
+   close(2)
+   
+end subroutine readMatrixFromFile
 program slice
       use, intrinsic :: iso_fortran_env
       implicit none
@@ -69,41 +84,56 @@ program slice
       integer i,j,index, maxa
 
 
-      real(kind=real64), dimension(6) :: coordinates 
+      real(kind=real64), dimension(6) :: coordinates, physcord
       real(kind=real64) canon(1:6), compare(1:6)
       real(kind=real64) identity2(1:6,1:6)
-      real(kind=real64) momentum, mass, one
-      real(kind=real64), dimension(6, 6) :: identity, results
+      real(kind=real64) momentum, mass, one, e1,e2, e3, betx1,zero, angle
+      real(kind=real64), dimension(6, 6) :: identity, results, testm, tas, emit
+      call readMatrixFromFile(tas)
+      e1 = 1.0d0
+      e2 = 2.0d0
+      e3 = 0.03d0 
+      emit(:,:) = 0.0
+      physcord(:) = 0.0
+      emit(1,1) = e1
+      emit(2,2) = e1
+      emit(3,3) = e2
+      emit(4,4) = e3
+
       maxa = 6
       one =1.0d0
+      zero = 0.0d0
       momentum = 5000.0
       mass = 3000.0
-      coordinates(1) = 1.0 
-      coordinates(2) = 2.0  
-      coordinates(3) = 3.0  
-      coordinates(4) = 4.0 
-      coordinates(5) = 10.0
-      coordinates(6) = 0.1
+      !print *, tas
+      do i=1,630
+        angle = 0.01*i
+        call a2c(e1, angle, e2, angle, e3, angle, tas, physcord)
+        print *, e1, angle, physcord(1), physcord(2),physcord(3), physcord(4),physcord(5), physcord(6)
+        physcord(:)= 0
+      end do
       identity(:,:) = 0
       results(:,:) = 0
+      testm(:,:) = 0
+      testm(2,3) = one
       identity(1,1) = one
       identity(2,2) = one
       identity(3,3) = one
       identity(4,4) = one
       identity(5,5) = one
       identity(6,6) = one
-      results(:,:)=0
+      results(:,:) = 0
       !call six2canonical(coordinates, momentum,mass, canon)
       !call canonical2six(canon, momentum, mass, compare)
 
       !print *, canon
       !print *, compare
       !print *, coordinates
-      print*, identity
+      !print*, identity
     
-      call mtrx_mult(maxa,maxa,maxa, identity, identity, results)
-      print *, "ouuttt"
-      print*, results(1,1),results(2,2),results(3,3)
+      !call mtrx_mult(maxa,maxa,maxa, identity, testm, results)
+      !print *, "ouuttt"
+      !print*, results(1,1),results(2,2),results(3,3), results(2,3)
       !call readtas
 ! Populate my 2D array with some values.
     !  index = 1
