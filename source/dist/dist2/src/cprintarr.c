006e1ad4a344642ac35656bedb5067c429b204d5
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-11-05 14:38:02 +0100

    More steps towards the generation of particles.

--- source/dist/dist2/src/cprintarr.c
+++ source/dist/dist2/src/cprintarr.c
@@ -2,23 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 #include "cprintarr.h"
-void
-cprintarr_(float *arr, int *nx, int *ny)
-{
-    int i, j;
-    printf("cprintarr_: arr=%p, nx=%d, ny=%d\n", arr, *nx, *ny);
-    for(j = 0; j < *ny; ++j)
-        for(i = 0; i < *nx; ++i)
-        {
-            int idx = j * *nx + i;
-            printf("arr[%d] = %f\n", idx, arr[idx]);
-        }
-}
-
-double momentum2energy(double momentum, double mass){
-	return sqrt(pow(momentum,2)+pow(mass,2));
-} 
-
 
 void 
 six2canonical_(double * coord, double *ref_momentum, double *mass, double *canonical){
@@ -72,15 +55,6 @@ void mtrx_vector_mult_(int *mp, int *np,  double mtrx_a[6][6], double mtrx_b[6],
                 
             }
     }
-
-}
-
-double test_(double in){
-	double ra[6];
-	ra[0]=0;
-	ra[2]=1;
-	printf("does this work %f ?", in );
-	return in;
 }
 
 
@@ -99,7 +73,40 @@ void a2c_(double *e1, double *a1, double *e2, double *a2, double *e3, double *a3
 	mtrx_vector_mult_(&dim,&dim, tas, acoord,results);
 
 }
-void createTasWithLinear(double betax, double alfax, double betay, double alfay, double tas[6][6]){
+void setEmittance(double *e1, double *e2, double *e3){
+
+}
+void initializeDistribution(int numberOfDist, int dim){
+
+}
+void setMass(double *mass){
+
+}
+
+void setMomentum(double *momentum){
+
+}
+void setJx(int *index,  double *start, double *stop, int *length, int *type){
+	struct parameters can;
+	can.start = *start;
+	can.stop = *stop;
+	can.length = *length;
+	can.type = *type;
+
+}
+
+
+double* createLinearSpaced(int length, double start, double stop){
+	double eqspaced [length];
+	double distance = (start-stop)/length;
+	for(int i; i<length; i++){
+		eqspaced[i] = distance*i;
+	}
+	return eqspaced;
+
+}
+
+void createTasWithNoCoupling(double betax, double alfax, double betay, double alfay, double tas[6][6]){
 
 	    for (int i = 0; i < 6; i++)
     {
@@ -109,7 +116,6 @@ void createTasWithLinear(double betax, double alfax, double betay, double alfay,
                 
             }
     }
-
     tas[0][0] = sqrt(betax);
     tas[2][2] = sqrt(betay);
     tas[1][2] =-alfax/sqrt(betax);
@@ -117,10 +123,39 @@ void createTasWithLinear(double betax, double alfax, double betay, double alfay,
 
 }
 
+double RationalApproximation(double t)
+{
+    // Abramowitz and Stegun formula 26.2.23.
+    // The absolute value of the error should be less than 4.5 e-4.
+    double c[] = {2.515517, 0.802853, 0.010328};
+    double d[] = {1.432788, 0.189269, 0.001308};
+    return t - ((c[2]*t + c[1])*t + c[0]) / 
+               (((d[2]*t + d[1])*t + d[0])*t + 1.0);
+}
+
+double normalcdfinv_(double p)
+{
+	printf("printing %f", p);
+    if (p <= 0.0 || p >= 1.0)
+    {
+    	return 0;
+    }
+
+    // See article above for explanation of this section.
+    if (p < 0.5)
+    {
+        // F^-1(p) = - G^-1(p)
+        return -RationalApproximation( sqrt(-2.0*log(p)) );
+    }
+    else
+    {
+        // F^-1(p) = G^-1(1-p)
+        return RationalApproximation( sqrt(-2.0*log(1-p)) );
+    }
+}
 
 void action2sixcoord_(double *e1, double *a1, double *e2, double *a2, double *e3, double *a3, double tas[6][6]
 	,double *ref_momentum, double *mass, double results [6]){
-
 	double cancord[6] ;
 	a2c_(e1, a1, e2, a2, e3, a3,tas, cancord);
 	canonical2six_(cancord, ref_momentum, mass, results);
