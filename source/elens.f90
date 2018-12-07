aa666aa3c6fb878c51fa05ef287c7674a0714520
amereghe <alessio.mereghetti@cern.ch>
2018-12-07 09:49:36 +0100

    open statements substituted with calls to units_openUnit

--- source/elens.f90
+++ source/elens.f90
@@ -504,6 +504,7 @@ subroutine parseRadialProfile(ifile)
   use mod_common
   use mod_settings
   use string_tools
+  use mod_units, only : units_openUnit
 
   implicit none
 
@@ -512,7 +513,7 @@ subroutine parseRadialProfile(ifile)
   character(len=:), allocatable   :: lnSplit(:)
   character(len=mInputLn) inLine
   integer nSplit
-  logical spErr
+  logical spErr, err
 
   integer iErr, ii
   real(kind=fPrec) tmpR, tmpJ
@@ -522,7 +523,7 @@ subroutine parseRadialProfile(ifile)
   elens_radial_profile_R(ii,ifile) = zero
   elens_radial_profile_J(ii,ifile) = zero
   write(lout,"(a)") "ELENS> Parsing file with radial profile "//trim(elens_radial_filename(ifile))
-  open(elens_radial_unit,file=elens_radial_filename(ifile),status="old")
+  call units_openUnit(unit=elens_radial_unit,fileName=elens_radial_filename(ifile),mode='r',err=err,status="old")
 
 10 continue
   read(elens_radial_unit,"(a)",end=20,err=30) inLine
@@ -673,6 +674,7 @@ subroutine parseChebyFile(ifile)
   use mod_common
   use mod_settings
   use string_tools
+  use mod_units, only : units_openUnit
 
   implicit none
 
@@ -681,14 +683,14 @@ subroutine parseChebyFile(ifile)
   character(len=:), allocatable   :: lnSplit(:)
   character(len=mInputLn) inLine
   integer nSplit
-  logical spErr
+  logical spErr,err
 
   integer iErr, ii, jj
   real(kind=fPrec) tmpflt, beta, gamma
 
   ierr = 0
   write(lout,"(a)") "ELENS> Parsing file with coefficients for Chebyshev polynomials "//trim(elens_cheby_filename(ifile))
-  open(elens_cheby_unit,file=elens_cheby_filename(ifile),status="old")
+  call units_openUnit(unit=elens_cheby_unit,fileName=elens_cheby_filename(ifile),mode='r',err=err,status="old")
 
 10 continue
   read(elens_cheby_unit,"(a)",end=20,err=30) inLine
