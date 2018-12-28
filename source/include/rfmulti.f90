34618e431ae471ab21559565f8953384de358414
Tobias <tpersson@cern.ch>
2018-12-28 13:13:01 +0100

    Symplectic and same result as MAD-X and old.

--- source/include/rfmulti.f90
+++ source/include/rfmulti.f90
@@ -5,56 +5,16 @@
   PNL = zero
   PSL = zero
 
-fact=1   
-do j=1,nordm-1   
-  fact=fact*j   
-end do
-
 
 do j=1,napx 
-  if(newstyle_rf .eq. 1) then
+
     irrtr = irm_rf(ix)
-    NORMAL = norrfamp(irrtr,:)
-    SKEW = skrfamp(irrtr,:)
-    PNL = norrfph(irrtr,:)
-    PSL = skrfph(irrtr,:)
     nordm=nmu_rf(irrtr)
-    crabfreq = freq_rfm(irrtr)
+    crabfreq = freq_rfm(irrtr)*c1e3
 
-  else
-    crabamp=ed(ix)*nzz(j)
-    NORMAL = zero
-    SKEW = zero
-    PNL = zero
-    PSL = zero
-
-    !nord = max(nn, ns, n_ferr/2-1)
-    if(isSkew .eq. 0) then 
-      if(nordm .eq. 1) then
-        pnl(1) = pi/2 - crabpase_t
-        normal(1) = crabamp/e0f
-      else
-        pnl(nordm) =  -crabpase_t
-        normal(nordm) = -crabamp*fact
-      endif
-    else
-      if(nordm .eq. 1) then
-        psl(1) = -pi/2 - crabpase_t
-        skew(1) = crabamp/e0f
-      else
-        psl(nordm) = - crabpase_t
-        skew(nordm) = crabamp*fact
-      endif
-    endif
-  endif
   
   krf = (((one/(clight*(e0f/e0)))*crabfreq)*two)*pi
   
-
-
-
-  print * , krf, crabfreq, crabamp, crabph(ix), pnl(1), normal(1), "ddddd"
-  print *, psl(1), skew(1), "skeeewww11"
 
     ! if zero:th order prepare
     ! if not zero change phase with a minus sign and no multiply
@@ -63,16 +23,12 @@ do j=1,napx
     x_t = crkve*c1m3 
     y_t = cikve*c1m3
     !---- Vector with strengths
-print *, "nooordm", nordm
     do iord = 1, nordm
-      print *, "sigmmmma", sigmv(j)
-      print *, "pnnnllll", pnl(iord)
-      print *, "streeeeength", normal(iord)
-      field_cos(1,iord) = (normal(iord) * cos(pnl(iord)*twopi  - krf * sigmv(j)))
-      field_sin(1,iord) = (normal(iord) * sin(pnl(iord)*twopi  - krf * sigmv(j)))
-      field_cos(2,iord) = (skew(iord)   * cos(psl(iord)*twopi  - krf * sigmv(j)))
-      field_sin(2,iord) = (skew(iord)   * sin(psl(iord)*twopi  - krf * sigmv(j)))
-      print *, "streeeeength", field_cos(1,iord)
+      field_cos(1,iord) = (norrfamp(irrtr,iord) * cos(norrfph(irrtr,iord)*twopi  - krf * sigmv(j)))
+      field_sin(1,iord) = (norrfamp(irrtr,iord) * sin(norrfph(irrtr,iord)*twopi  - krf * sigmv(j)))
+      field_cos(2,iord) = (skrfamp(irrtr,iord)   * cos(skrfph(irrtr,iord)*twopi  - krf * sigmv(j)))
+      field_sin(2,iord) = (skrfamp(irrtr,iord)   * sin(skrfph(irrtr,iord)*twopi  - krf * sigmv(j)))
+    
     enddo
     Cm2 = zero; Sm2a = zero; Cm1 = zero; Sm1a = zero;
     Cp0 = zero; Sp0 = zero; Cp1 = zero; Sp1 = zero;
@@ -80,8 +36,6 @@ print *, "nooordm", nordm
     do iord = nordm, 1, -1
 
       Cp0 = Cp0 * (x_t+imag*y_t) / (iord)   + field_cos(1,iord)+imag*field_cos(2,iord);
-      !Sp0 = Sp0 * (x_t+imag*y_t) / (iord+1)   + field_sin(1,iord)+imag*field_sin(2,iord);
-      !Cp1 = Cp1 * (x_t+imag*y_t) / (iord+2)   + field_cos(1,iord)+imag*field_cos(2,iord);
       Sp1 = Sp1 * (x_t+imag*y_t) / (iord+1)   + field_sin(1,iord)+imag*field_sin(2,iord);
     enddo
     Sp1 = Sp1 * (x_t+imag*y_t);
@@ -89,15 +43,12 @@ print *, "nooordm", nordm
 
     dpx = -REAL(Cp0)*c1e3*moidpsv(j);
     dpy = AIMAG(Cp0)*c1e3*moidpsv(j);
-    dpt = - (REAL(Sp1)*c1e6*e0f*crabfreq*two*pi)/clight;
+    dpt = - (REAL(Sp1)*c1e3*e0f*crabfreq*two*pi)/clight;
 
     yv1(j) = yv1(j) + dpx
-    yv2(j) = yv2(j) + dpy
-    print *, "ejjjbefore", ejv(j)
+    yv2(j) = yv2(j) + dpy 
     ejv(j) = ejv(j) + dpt
-    print *, "ejjjafter", ejv(j)
 
-    
 
     ejf0v(j)=ejfv(j)
     ejfv(j)=sqrt(ejv(j)**2-nucm(j)**2)
@@ -111,7 +62,6 @@ print *, "nooordm", nordm
     yv2(j)=(ejf0v(j)/ejfv(j))*yv2(j)
     if(ithick.eq.1) call envarsv(dpsv,moidpsv,rvv,ekv)
 
-    print *, "aaaaa", dpx, dpy, dpt, Sp1, imag
   end do
 
 
--- source/include/rfmulti_fox.f90
+++ source/include/rfmulti_fox.f90
@@ -3,16 +3,13 @@
 
 irrtr = irm_rf(ix)
 nordm=nmu_rf(irrtr)
-crabfreq = freq_rfm(irrtr)  
+crabfreq = freq_rfm(irrtr)*c1e3  
 
 !FOX  KCRABDA=(SIGMDA/(CLIGHT*(E0F/E0))
 !FOX  *CRABFREQ*2D0*PI) ;
     
 !FOX  DXRF=XL*C1M3 ;
 !FOX  DYRF=ZL*C1M3 ;
-print *, "crabdaaaaa", dare(kcrabda)
-print *, "DXRFaaaa", dare(dxrf)
-print *, "DYRFaaaa", dare(dyrf)
 
  do iord = 1, nordm
 !FOX  FCODA(1, IORD)=(NORRFAMP(IRRTR, IORD)* 
@@ -26,11 +23,6 @@ print *, "DYRFaaaa", dare(dyrf)
 
 !FOX  FSIDA(2, IORD)=(SKRFAMP(IRRTR, IORD)*
 !FOX  SIN(SKRFPH(IRRTR, IORD)*2D0*PI  - KCRABDA)) ;
-print * , "irrrtr", irrtr
-print *, "sida1daaaaa", iord, dare(fsida(1,iord))
-print *, "sida1daaaaa",iord, dare(fsida(2,iord))
-print *, "sida1daaaaa",iord, dare(fcoda(1,iord))
-print *, "sida1daaaaa",iord, dare(fcoda(2,iord))
 
 enddo
 
@@ -38,8 +30,8 @@ enddo
 !FOX  CP_IM=ZERO ;
 !FOX  SP_RE=ZERO ;
 !FOX  SP_IM=ZERO ;
+
 do iord = nordm, 1, -1
-! Måste fixaaaa temp variablens!!!
 !FOX  CP_RETP = CP_RE ;
 !FOX  CP_RE=(CP_RETP*DXRF - CP_IM*DYRF)/(IORD)  + FCODA(1, IORD) ;
 !FOX  CP_IM=(CP_RETP*DYRF + CP_IM*DXRF)/(IORD)  + FCODA(2, IORD) ;
@@ -47,35 +39,17 @@ do iord = nordm, 1, -1
 !FOX  SP_RETP = SP_RE ;
 !FOX  SP_RE=(SP_RETP*DXRF - SP_IM*DYRF)/(IORD+1)  + FSIDA(1, IORD) ;
 !FOX  SP_IM=(SP_RETP*DYRF + SP_IM*DXRF)/(IORD+1)  + FSIDA(2, IORD) ;
-print *, "sida1daaaaapppppsp_resp", dare(sp_re)
-print *, "sida1daaaaapppppsp_resp", dare(sp_im)
-
-print *, "sida1daaaaapppppcp_recp", dare(cp_re)
-print *, "sida1daaaaapppppcp_recp", dare(cp_im)
 enddo
 
 !FOX  SP_RETP = SP_RE ;
 !FOX  SP_RE=(SP_RETP*DXRF - SP_IM*DYRF) ;
 !FOX  SP_IM=(SP_RETP*DYRF + SP_IM*DXRF) ;
 
-print *, "sida1daaaaapppppsp_re_after", dare(sp_re)
-print *, "sida1daaaaapppppsp_re_after", dare(sp_im)
-
-print *, "sida1daaaaapppppsp_re_aftercp", dare(cp_re)
-print *, "sida1daaaaapppppsp_re_aftercp", dare(cp_im)
 !FOX  Y(1)=Y(1) - CP_RE*C1E3*MTCDA/(ONE+DPDA);
 !FOX  Y(2)=Y(2) + CP_IM*C1E3*MTCDA/(ONE+DPDA);
-print *, "sida1daaaaapppppsp_y1", dare(y(1))
-print *, "sida1daaaaapppppsp_y2", dare(y(2))
 
-print *, "ejjjjj111before", dare(ej1)
 !FOX  EJ1 = EJ1 - (C1E3*E0F*SP_RE*CRABFREQ*2D0*PI)
 !FOX  /(CLIGHT) ;
-
-print *, "ejjjjj111", dare(ej1)
-
-print *, "sida1daaaaapppppsp_y1", dare(y(1))
-print *, "sida1daaaaapppppsp_y2", dare(y(2))
 
 !FOX  EJF0=EJF1 ;
 !FOX  EJF1=SQRT(EJ1*EJ1-NUCMDA*NUCMDA) ;
@@ -86,10 +60,3 @@ print *, "sida1daaaaapppppsp_y2", dare(y(2))
 !FOX  MOIDA=MTCDA/(ONE+DPDA) ;
 !FOX  Y(1)=EJF0/EJF1*Y(1) ;
 !FOX  Y(2)=EJF0/EJF1*Y(2) ;
-
-
-
-   
-
-
-
