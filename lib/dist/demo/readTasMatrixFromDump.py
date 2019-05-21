c53890eef5c730f1e41e8db41889f82ad8f81545
tobias <tobias@pcbe15807.dyndns.cern.ch>
2019-05-21 09:49:59 +0200

    added test file

---
+++ lib/dist/demo/readTasMatrixFromDump.py
@@ -0,0 +1,75 @@
+import numpy as np
+from ctypes import *
+import matplotlib.pyplot as plt
+def readtasfromdump(filename):
+	file = open(filename, "r")
+	splitlines = (file.readlines())
+	tasstr = (str.split(splitlines[3]))
+	tasstr = np.array(tasstr[3:])
+	tas = tasstr.astype(np.float)
+	tasm = tas.reshape(6,6)
+	return tasm
+
+def settasmatrix(dist, tas):
+	for i in range(0,6):
+		for j in range(0,6):
+			dist.settasmatrix_element(c_double(tas[i][j]), c_int(i), c_int(j))
+
+def setParameters(dist, index, start, stop, numb, type):
+	dist.setparameter_(byref(c_int(index)), byref(c_double(start)),byref(c_double(stop)),byref(c_int(numb)),byref(c_int(type)))
+
+
+dist = cdll.LoadLibrary("./buildDemo/libhello.so")
+dist.initializedistribution_(byref(c_int(2)))
+
+myfile = "/home/tobias/codes/SixTrackTobias/test/orbit6d-element-quadrupole/START_DUMP"
+tas = readtasfromdump(myfile)
+settasmatrix(dist, tas)
+
+
+
+double6 = c_double * 6
+physical = double6(0,0,0,0,0,0)
+pia2 = np.pi*2
+momentum = c_double(6500000)
+mass = c_double(938.0)
+eps = 2.0
+dim = c_int(6)
+zero = c_double(0)
+e1 = c_double(eps)
+e2 = c_double(1.0)
+e3 = c_double(0.0)
+dist.setemittance12_(byref(e1),byref(e2))
+dist.setemittance3_(byref(e3))
+dist.setmassmom_(byref(mass), byref(momentum))
+dist.setdisttype(c_int(1))
+
+
+dist.settotallength(c_int(2000))
+setParameters(dist,1,0,4,200,6)
+setParameters(dist,2,0,pia2,200,4)
+setParameters(dist,3,0,0,1,0)
+setParameters(dist,4,0,0,1,0)
+setParameters(dist,5,0,0,1,0)
+setParameters(dist,6,0,0,1,0)
+
+dist.printdistsettings_()
+dist.print2file()
+
+xd = []
+pxd = []
+yd = []
+yxd = []
+print("inpython")
+print("aaa", dist.getnumberdist_())
+for i in range(0,dist.getnumberdist_()):
+	dist.getcoord_(physical,c_int(i))
+	xd.append(physical[0])
+	pxd.append(physical[1])
+	yd.append(physical[2])
+	yxd.append(physical[3])
+	print(physical[0])
+
+
+plt.plot(xd, pxd, '*')
+plt.show()
