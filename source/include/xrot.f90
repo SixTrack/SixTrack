cabd9b10b1550a4006f80877c4abb79e5751561a
tobias <tobias@pcbe15807.dyndns.cern.ch>
2019-05-29 18:04:10 +0200

    The xrotation for tracking..

---
+++ source/include/xrot.f90
@@ -0,0 +1,33 @@
+
+xlv = 0.2
+
+costh_temp = cos(xlv)
+sinth_temp = sin(xlv)
+tan_temp = tan(xlv)
+do j=1,napx
+temptr(1)=c1m3*xv1(j)
+temptr(2)=c1m3*yv1(j)/moidpsv(j)
+temptr(3)=c1m3*xv2(j)
+temptr(4)=c1m3*yv2(j)/moidpsv(j)
+temptr(5)=c1m3*sigmv(j)/(e0f/e0)
+temptr(6)=c1m3*((mtc(j)*ejv(j)-e0)/e0f)
+print *, temptr, "uuuu" 
+!x  = TRACK(1,i)
+!px = TRACK(2,i)
+!y  = TRACK(3,i)
+!py = TRACK(4,i)
+!t  = TRACK(5,i)
+!pt = TRACK(6,i)
+
+z_temp = sqrt(one + two*temptr(6)/(e0f/e0) + temptr(6)**2 - temptr(2)**2 - temptr(4)**2)
+pttemp = 1 - tan_temp*temptr(4)/z_temp
+
+print *, z_temp, pttemp, "vvvv"
+
+xv1(j) = xv1(j) + c1e3*(tan_temp*temptr(3)*temptr(2)/(z_temp*pttemp))
+xv2(j) = c1e3*temptr(3)/(costh_temp*pttemp)
+yv2(j) = c1e3*(costh_temp*temptr(4) + sinth_temp*z_temp)
+sigmv(j) = sigmv(j) - c1e3*((tan_temp*temptr(3)*(one/(e0f/e0)+temptr(6))/(z_temp*pttemp))*(e0f/e0))
+
+print * , "xxxx", xv1(j), "px", xv2(j), yv2(j), sigmv(j)
+enddo
