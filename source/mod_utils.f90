ab9d581ff17d5213dd29cc7e626a34f610010460
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-08-29 14:33:14 +0200

    Move the wzsub, wzset routines to mod_utils

--- source/mod_utils.f90
+++ source/mod_utils.f90
@@ -153,4 +153,492 @@ subroutine errf(xx, yy, wx, wy)
 
 end subroutine errf
 
+subroutine wwerf(x,y,wr,wi)
+  use floatPrecision
+  use mathlib_bouncer
+  use numerical_constants
+  implicit none
+  integer n
+  real(kind=fPrec) c,c1,c2,c3,c4,hf,p,rr,ri,sr0,sr,si,tr,ti,vi,vr,  &
+ &wi,wr,x,xa,xl,y,ya,zhi,zhr,z1,z10
+  parameter (z1=one,hf=z1/two,z10=c1e1)
+  parameter (c1=74.0_fPrec/z10,c2=83.0_fPrec/z10)
+  parameter (c3=z10/32.0_fPrec,c4=16.0_fPrec/z10)
+!     parameter (c=1.12837916709551257d0,p=(2d0*c4)**33)
+  parameter (c=1.12837916709551257_fPrec)
+  parameter (p=46768052394588893.3825_fPrec)
+  dimension rr(37),ri(37)
+  save
+!-----------------------------------------------------------------------
+  xa=abs(x)
+  ya=abs(y)
+  if(ya.lt.c1.and.xa.lt.c2) then
+!        zh=dcmplx(ya+c4,xa)
+    zhr=ya+c4
+    zhi=xa
+    rr(37)=zero
+    ri(37)=zero
+    do n=36,1,-1
+!          t=zh+n*dconjg(r(n+1))
+      tr=zhr+real(n,fPrec)*rr(n+1)
+      ti=zhi-real(n,fPrec)*ri(n+1)
+!          r(n)=hf*t/(dreal(t)**2+dimag(t)**2)
+      rr(n)=(hf*tr)/(tr**2+ti**2)
+      ri(n)=(hf*ti)/(tr**2+ti**2)
+    enddo
+    xl=p
+    sr=zero
+    si=zero
+    do n=33,1,-1
+      xl=c3*xl
+!          s=r(n)*(s+xl)
+      sr0=rr(n)*(sr+xl)-ri(n)*si
+      si=rr(n)*si+ri(n)*(sr+xl)
+      sr=sr0
+    enddo
+!        v=c*s
+    vr=c*sr
+    vi=c*si
+  else
+    zhr=ya
+    zhi=xa
+    rr(1)=zero
+    ri(1)=zero
+    do n=9,1,-1
+!          t=zh+n*dconjg(r(1))
+      tr=zhr+real(n,fPrec)*rr(1)
+      ti=zhi-real(n,fPrec)*ri(1)
+!          r(1)=hf*t/(dreal(t)**2+dimag(t)**2)
+      rr(1)=(hf*tr)/(tr**2+ti**2)
+      ri(1)=(hf*ti)/(tr**2+ti**2)
+    enddo
+!        v=c*r(1)
+    vr=c*rr(1)
+    vi=c*ri(1)
+  endif
+  if(ya.eq.zero) then
+!        v=dcmplx(exp(-xa**2),dimag(v))
+    vr=exp_mb(-one*xa**2)
+  endif
+  if(y.lt.zero) then
+!        v=2*exp(-dcmplx(xa,ya)**2)-v
+    vr=(two*exp_mb(ya**2-xa**2))*cos_mb((two*xa)*ya)-vr
+    vi=(-two*exp_mb(ya**2-xa**2))*sin_mb((two*xa)*ya)-vi
+    if(x.gt.zero) vi=-one*vi
+  else
+    if(x.lt.zero) vi=-one*vi
+  endif
+  wr=vr
+  wi=vi
+  return
+end subroutine wwerf
+
+! ================================================================================================ !
+!  subroutine wzsubv
+!
+!  This subroutine sets u=real(w(z)) and v=imag(w(z)), where z=x+i*y and
+!  where w(z) is the complex error function defined by formula 7.1.3 in
+!  "Handbook of Mathematical functions [eds. M.Abramowitz & I.A.Stegun,
+!  Washington, 1966].  The absolute error of the computed value is less
+!  than 1E-8.
+!
+!  *** Note.  Subroutine WZSET must have been called before this sub-
+!  routine can be used.
+!
+!  For (x,y) inside the rectangle with opposite corners (xcut,0) and
+!  (0,ycut), where xcut and ycut have been set by WZSET, an interpo-
+!  lation formula is used.  For (x,y) outside this rectangle, a two-
+!  term rational approximation is used.
+!
+!  (G.A.Erskine, 29.09.1997)
+!
+!  Vectorised for up to 64 argument values by E.McIntosh, 30.10.1997.
+!  Much impoved using short vector buffers Eric 1st May, 2014.
+!
+!  Third-order divided-difference interpolation over the corners of a
+!  square [e.g. formula (2.5.1) in "Introduction to Numerical Analysis"
+!  (F.B.Hildebrand New York, 1957), but with complex nodes and
+!  function values].
+!
+!  In the interpolation formula the corners of the grid square contain-
+!  ing (x,y) are numbered (0,0)=3, (h,0)=4, (h,h)=1, (0,h)=2.
+!  Identifiers d, dd and ddd denote divided-differences of orders 1, 2
+!  and 3 respectively, and a preceding 't' indicates twice the value.
+!
+!
+!  Two-term rational approximation to w(z) [Footnote to Table 7.9
+!  in "Handbook of Mathematical Functions (eds. M.Abramowitz &
+!  I.A.Stegun, Washington, 1966), but with additional digits in
+!  the constants]:
+!              u+i*v = i*z*( a1/(z**2-b1) + a2/(z**2-b2) ).
+!  Maximum absolute error:
+!        <1.E-6  for  x>=4.9  or  y>=4.4
+!        <1.E-7  for  x>=6.1  or  y>=5.7
+!        <1.E-8  for  x>=7.8  or  y>=7.5
+!
+! ================================================================================================ !
+subroutine wzsubv(n,vx,vy,vu,vv)
+
+  use parpro, only : npart
+  use floatPrecision
+  use numerical_constants
+  implicit none
+
+  dimension vx(*),vy(*),vu(*),vv(*)
+  integer i,j,k,n,vmu,vnu
+  real(kind=fPrec) a1,a2,b1,b2,vd12i,vd12r,vd23i,vd23r,vd34i,vd34r,vp,vq,vqsq,vr,vsimag,vsreal,vt,  &
+    vtdd13i,vtdd13r,vtdd24i,vtdd24r,vtdddi,vtdddr,vti,vtr,vu,vusum,vusum3,vv,vvsum,vvsum3,vw1i,vw1r,&
+    vw2i,vw2r,vw3i,vw3r,vw4i,vw4r,vx,vxh,vxhrel,vy,vyh,vyhrel
+  integer idim,kstep,nx,ny
+  real(kind=fPrec) h,hrecip,wtimag,wtreal,xcut,ycut
+  parameter ( xcut = 7.77_fPrec, ycut = 7.46_fPrec )
+  parameter ( h = one/63.0_fPrec )
+  parameter ( nx = 490, ny = 470 )
+  parameter ( idim = (nx+2)*(ny+2) )
+  common /wzcom1/ hrecip, kstep
+  common /wzcom2/ wtreal(idim), wtimag(idim)
+  parameter ( a1 = 0.5124242248_fPrec, a2 = 0.0517653588_fPrec )
+  parameter ( b1 = 0.2752551286_fPrec, b2 = 2.7247448714_fPrec )
+  real(kind=fPrec) xm,xx,yy
+  parameter (xm=1e16_fPrec)
+!     temporary arrays to facilitate vectorisation
+  integer in,out,ins,outs
+  dimension ins(npart),outs(npart)
+!-----------------------------------------------------------------------
+  save
+  in=0
+  out=0
+  do i=1,n
+    if (vx(i).ge.xcut.or.vy(i).ge.ycut) then
+      out=out+1
+      outs(out)=i
+      if (out.eq.npart) then
+!     everything outside the rectangle so approximate
+!     write (*,*) 'ALL outside'
+!     write (*,*) 'i=',i
+        do j=1,out
+          xx=vx(outs(j))
+          yy=vy(outs(j))
+          if (xx.ge.xm) xx=xm
+          if (yy.ge.xm) yy=xm
+          vp=xx**2-yy**2
+          vq=(two*xx)*yy
+          vqsq=vq**2
+          !  First term.
+          vt=vp-b1
+          vr=a1/(vt**2+vqsq)
+          vsreal=vr*vt
+          vsimag=-vr*vq
+          !  Second term
+          vt=vp-b2
+          vr=a2/(vt**2+vqsq)
+          vsreal=vsreal+vr*vt
+          vsimag=vsimag-vr*vq
+          !  Multiply by i*z.
+          vu(outs(j))=-(yy*vsreal+xx*vsimag)
+          vv(outs(j))=xx*vsreal-yy*vsimag
+        enddo
+        out=0
+      endif
+    else
+      in=in+1
+      ins(in)=i
+      if (in.eq.npart) then
+!     everything inside the square, so interpolate
+!     write (*,*) 'ALL inside'
+        do j=1,in
+          vxh = hrecip*vx(ins(j))
+          vyh = hrecip*vy(ins(j))
+          vmu = int(vxh)
+          vnu = int(vyh)
+!  Compute divided differences.
+          k = 2 + vmu + vnu*kstep
+          vw4r = wtreal(k)
+          vw4i = wtimag(k)
+          k = k - 1
+          vw3r = wtreal(k)
+          vw3i = wtimag(k)
+          vd34r = vw4r - vw3r
+          vd34i = vw4i - vw3i
+          k = k + kstep
+          vw2r = wtreal(k)
+          vw2i = wtimag(k)
+          vd23r = vw2i - vw3i
+          vd23i = vw3r - vw2r
+          vtr = vd23r - vd34r
+          vti = vd23i - vd34i
+          vtdd24r = vti - vtr
+          vtdd24i = -one* ( vtr + vti )
+          k = k + 1
+          vw1r = wtreal(k)
+          vw1i = wtimag(k)
+          vd12r = vw1r - vw2r
+          vd12i = vw1i - vw2i
+          vtr = vd12r - vd23r
+          vti = vd12i - vd23i
+          vtdd13r = vtr + vti
+          vtdd13i = vti - vtr
+          vtdddr = vtdd13i - vtdd24i
+          vtdddi = vtdd24r - vtdd13r
+!  Evaluate polynomial.
+          vxhrel = vxh - real(vmu,fPrec)
+          vyhrel = vyh - real(vnu,fPrec)
+          vusum3=half*(vtdd13r+(vxhrel*vtdddr-vyhrel*vtdddi))
+          vvsum3=half*(vtdd13i+(vxhrel*vtdddi+vyhrel*vtdddr))
+          vyhrel = vyhrel - one
+          vusum=vd12r+(vxhrel*vusum3-vyhrel*vvsum3)
+          vvsum=vd12i+(vxhrel*vvsum3+vyhrel*vusum3)
+          vxhrel = vxhrel - one
+          vu(ins(j))=vw1r+(vxhrel*vusum-vyhrel*vvsum)
+          vv(ins(j))=vw1i+(vxhrel*vvsum+vyhrel*vusum)
+        enddo
+        in=0
+      endif
+    endif
+  enddo
+!     everything outside the rectangle so approximate
+!     write (*,*) 'ALL outside'
+!     write (*,*) 'i=',i
+  do j=1,out
+    xx=vx(outs(j))
+    yy=vy(outs(j))
+    if (xx.ge.xm) xx=xm
+    if (yy.ge.xm) yy=xm
+    vp=xx**2-yy**2
+    vq=(two*xx)*yy
+    vqsq=vq**2
+!  First term.
+    vt=vp-b1
+    vr=a1/(vt**2+vqsq)
+    vsreal=vr*vt
+    vsimag=-vr*vq
+!  Second term
+    vt=vp-b2
+    vr=a2/(vt**2+vqsq)
+    vsreal=vsreal+vr*vt
+    vsimag=vsimag-vr*vq
+!  Multiply by i*z.
+    vu(outs(j))=-(yy*vsreal+xx*vsimag)
+    vv(outs(j))=xx*vsreal-yy*vsimag
+  enddo
+!     everything inside the square, so interpolate
+!     write (*,*) 'ALL inside'
+  do j=1,in
+    vxh = hrecip*vx(ins(j))
+    vyh = hrecip*vy(ins(j))
+    vmu = int(vxh)
+    vnu = int(vyh)
+!  Compute divided differences.
+    k = 2 + vmu + vnu*kstep
+    vw4r = wtreal(k)
+    vw4i = wtimag(k)
+    k = k - 1
+    vw3r = wtreal(k)
+    vw3i = wtimag(k)
+    vd34r = vw4r - vw3r
+    vd34i = vw4i - vw3i
+    k = k + kstep
+    vw2r = wtreal(k)
+    vw2i = wtimag(k)
+    vd23r = vw2i - vw3i
+    vd23i = vw3r - vw2r
+    vtr = vd23r - vd34r
+    vti = vd23i - vd34i
+    vtdd24r = vti - vtr
+    vtdd24i = -one* ( vtr + vti )
+    k = k + 1
+    vw1r = wtreal(k)
+    vw1i = wtimag(k)
+    vd12r = vw1r - vw2r
+    vd12i = vw1i - vw2i
+    vtr = vd12r - vd23r
+    vti = vd12i - vd23i
+    vtdd13r = vtr + vti
+    vtdd13i = vti - vtr
+    vtdddr = vtdd13i - vtdd24i
+    vtdddi = vtdd24r - vtdd13r
+!  Evaluate polynomial.
+    vxhrel = vxh - real(vmu,fPrec)
+    vyhrel = vyh - real(vnu,fPrec)
+    vusum3=half*(vtdd13r+(vxhrel*vtdddr-vyhrel*vtdddi))
+    vvsum3=half*(vtdd13i+(vxhrel*vtdddi+vyhrel*vtdddr))
+    vyhrel = vyhrel - one
+    vusum=vd12r+(vxhrel*vusum3-vyhrel*vvsum3)
+    vvsum=vd12i+(vxhrel*vvsum3+vyhrel*vusum3)
+    vxhrel = vxhrel - one
+    vu(ins(j))=vw1r+(vxhrel*vusum-vyhrel*vvsum)
+    vv(ins(j))=vw1i+(vxhrel*vvsum+vyhrel*vusum)
+  enddo
+  return
+end subroutine wzsubv
+
+! ================================================================================================ !
+!  subroutine wzsub
+!
+!  This subroutine sets u=real(w(z)) and v=imag(w(z)), where z=x+i*y and
+!  where w(z) is the complex error function defined by formula 7.1.3 in
+!  "Handbook of Mathematical functions [eds. M.Abramowitz & I.A.Stegun,
+!  Washington, 1966].  The absolute error of the computed value is less
+!  than 1E-8.
+!
+!  *** Note.  Subroutine WZSET must have been called before this sub-
+!  routine can be used.
+!
+!  For (x,y) inside the rectangle with opposite corners (xcut,0) and
+!  (0,ycut), where xcut and ycut have been set by WZSET, an interpo-
+!  lation formula is used.  For (x,y) outside this rectangle, a two-
+!  term rational approximation is used.
+!
+!  (G.A.Erskine, 29.09.1997)
+!
+!
+!  Third-order divided-difference interpolation over the corners of a
+!  square [e.g. formula (2.5.1) in "Introduction to Numerical Analysis"
+!  (F.B.Hildebrand New York, 1957), but with complex nodes and
+!  function values].
+!
+!  In the interpolation formula the corners of the grid square contain-
+!  ing (x,y) are numbered (0,0)=3, (h,0)=4, (h,h)=1, (0,h)=2.
+!  Identifiers d, dd and ddd denote divided-differences of orders 1, 2
+!  and 3 respectively, and a preceding 't' indicates twice the value.
+!
+! ================================================================================================ !
+subroutine wzsub(x,y,u,v)
+
+  use floatPrecision
+  use numerical_constants
+  use mathlib_bouncer
+  use parpro
+  use parbeam
+  implicit none
+  integer k,mu,nu
+  real(kind=fPrec) a1,a2,b1,b2,d12i,d12r,d23i,d23r,d34i,d34r,p,q,qsq,r,simag,sreal,t,tdd13i,tdd13r, &
+    tdd24i,tdd24r,tdddi,tdddr,ti,tr,u,usum,usum3,v,vsum,vsum3,w1i,w1r,w2i,w2r,w3i,w3r,w4i,w4r,x,xh, &
+    xhrel,y,yh,yhrel
+  parameter ( a1 = 0.5124242248_fPrec, a2 = 0.0517653588_fPrec )
+  parameter ( b1 = 0.2752551286_fPrec, b2 = 2.7247448714_fPrec )
+  save
+!-----------------------------------------------------------------------
+  if ( x.ge.xcut .or. y.ge.ycut ) goto 1000
+  xh = hrecip*x
+  yh = hrecip*y
+  mu = int(xh)
+  nu = int(yh)
+!  Compute divided differences.
+  k = 2 + mu + nu*kstep
+  w4r = wtreal(k)
+  w4i = wtimag(k)
+  k = k - 1
+  w3r = wtreal(k)
+  w3i = wtimag(k)
+  d34r = w4r - w3r
+  d34i = w4i - w3i
+  k = k + kstep
+  w2r = wtreal(k)
+  w2i = wtimag(k)
+  d23r = w2i - w3i
+  d23i = w3r - w2r
+  tr = d23r - d34r
+  ti = d23i - d34i
+  tdd24r = ti - tr
+  tdd24i = -one* ( tr + ti )
+  k = k + 1
+  w1r = wtreal(k)
+  w1i = wtimag(k)
+  d12r = w1r - w2r
+  d12i = w1i - w2i
+  tr = d12r - d23r
+  ti = d12i - d23i
+  tdd13r = tr + ti
+  tdd13i = ti - tr
+  tdddr = tdd13i - tdd24i
+  tdddi = tdd24r - tdd13r
+!  Evaluate polynomial.
+  xhrel = xh - real(mu,fPrec)
+  yhrel = yh - real(nu,fPrec)
+  usum3 = half*( tdd13r + ( xhrel*tdddr - yhrel*tdddi ) )
+  vsum3 = half*( tdd13i + ( xhrel*tdddi + yhrel*tdddr ) )
+  yhrel = yhrel - one
+  usum = d12r + ( xhrel*usum3 - yhrel*vsum3 )
+  vsum = d12i + ( xhrel*vsum3 + yhrel*usum3 )
+  xhrel = xhrel - one
+  u = w1r + ( xhrel*usum - yhrel*vsum )
+  v = w1i + ( xhrel*vsum + yhrel*usum )
+  return
+!
+!  Two-term rational approximation to w(z) [Footnote to Table 7.9
+!  in "Handbook of Mathematical Functions (eds. M.Abramowitz &
+!  I.A.Stegun, Washington, 1966), but with additional digits in
+!  the constants]:
+!              u+i*v = i*z*( a1/(z**2-b1) + a2/(z**2-b2) ).
+!  Maximum absolute error:
+!        <1.E-6  for  x>=4.9  or  y>=4.4
+!        <1.E-7  for  x>=6.1  or  y>=5.7
+!        <1.E-8  for  x>=7.8  or  y>=7.5
+!
+1000 p=x**2-y**2
+  q=(2.d0*x)*y
+  qsq=q**2
+!  First term.
+  t=p-b1
+  r=a1/(t**2+qsq)
+  sreal=r*t
+  simag=(-one*r)*q
+!  Second term
+  t=p-b2
+  r=a2/(t**2+qsq)
+  sreal=sreal+r*t
+  simag=simag-r*q
+!  Multiply by i*z.
+  u=-one*(y*sreal+x*simag)
+  v=x*sreal-y*simag
+  return
+!
+end subroutine wzsub
+
+!  *********************************************************************
+!
+!  This subroutine must be called before subroutine WZSUB can be used to
+!  compute values of the complex error function w(z).
+!
+!  Parameters xcut and ycut specify the opposite corners (xcut,0) and
+!  (0,ycut) of the rectangle inside which interpolation is to be used
+!  by subroutine WZSUB.
+!
+!  Parameter h is the side of the squares of the interpolation grid.
+!
+!  Parameters nx and ny must be set to the nearest integers to xcut/h
+!  and ycut/h respectively (or to larger values).
+!
+!  Calls wwerf new version of (CERN library) WWERF (C335)
+!
+!  (G.A.Erskine, 29.09.1995)
+!
+!  *********************************************************************
+subroutine wzset
+  use floatPrecision
+      use mathlib_bouncer
+      use numerical_constants
+      use parpro
+      use parbeam
+      implicit none
+      integer i,j,k
+      real(kind=fPrec) wi,wr,x,y
+      save
+!-----------------------------------------------------------------------
+      hrecip = one/h
+      kstep = nx+2
+      k = 0
+      do 2 j=0,ny+1
+         do 1 i=0,nx+1
+            k = k+1
+            x=real(i,fPrec)*h
+            y=real(j,fPrec)*h
+            call wwerf(x,y,wr,wi)
+            wtreal(k)=wr
+            wtimag(k)=wi
+ 1       continue
+ 2    continue
+end subroutine wzset
+
 end module mod_utils
