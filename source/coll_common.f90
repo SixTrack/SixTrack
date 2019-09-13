f556992df98e902ab0d3cbf70d849ba31eaf5d1a
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-09-13 11:07:11 +0200

    Added coll_common and coll_k2 modules

---
+++ source/coll_common.f90
@@ -0,0 +1,14 @@
+! ============================================================================ !
+!  Collimation Common Variables
+! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+! ============================================================================ !
+module coll_common
+
+  use floatPrecision
+  use numerical_constants
+
+  implicit none
+
+contains
+
+end module coll_common
---
+++ source/coll_k2.f90
@@ -0,0 +1,14 @@
+! ============================================================================ !
+!  Collimation K2 Physics Module
+! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+! ============================================================================ !
+module coll_k2
+
+  use floatPrecision
+  use numerical_constants
+
+  implicit none
+
+contains
+
+end module coll_k2
