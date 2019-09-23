66525a84c605733fbb71ae2cbf47e04be58d1f76
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-09-23 09:31:23 +0200

    Added random numbers module

---
+++ source/mod_random.f90
@@ -0,0 +1,22 @@
+! ================================================================================================ !
+!  Random Number Handling
+! ~~~~~~~~~~~~~~~~~~~~~~~~
+!  Module for handling random numbers and keeping track of seeds for continuation of random
+!  number generatio. The module keeps multiple series of random numbers separate between the
+!  different modules of SixTrack.
+!
+!  V.K. Berglyd Olsen, BE-ABP-HSS
+!  Created: 2019-09-23
+!  Updated: 2019-09-23
+! ================================================================================================ !
+module mod_random
+
+  use floatPrecision
+
+  implicit none
+
+contains
+
+
+
+end module mod_random
