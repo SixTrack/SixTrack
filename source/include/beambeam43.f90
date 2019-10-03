c5674a19f0e78fda7d515b9a1d02979a89c136c6
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-10-03 20:21:56 +0200

    Optimised beambeam43

--- source/include/beambeam43.f90
+++ source/include/beambeam43.f90
@@ -1,23 +1,20 @@
 ! start include/beambeam43.f90
-do j=1,napx
-  r2b(j) = two*(sigman2(2,imbb(i))-sigman2(1,imbb(i)))
-  rb(j)  = sqrt(r2b(j))
-  rkb(j) = (strack(i)*pisqrt)/rb(j)
-  if(ibbc == 0) then
-    crkveb(j) = (xv1(j)-clobeam(1,imbb(i)))+parbe(ix,5)
-    cikveb(j) = (xv2(j)-clobeam(2,imbb(i)))+parbe(ix,6)
-  else
-    crkveb(j) = ((xv1(j)-clobeam(1,imbb(i)))+parbe(ix,5))*bbcu(imbb(i),11)&
-              + ((xv2(j)-clobeam(2,imbb(i)))+parbe(ix,6))*bbcu(imbb(i),12)
-    cikveb(j) = ((xv2(j)-clobeam(2,imbb(i)))+parbe(ix,6))*bbcu(imbb(i),11)&
-              - ((xv1(j)-clobeam(1,imbb(i)))+parbe(ix,5))*bbcu(imbb(i),12)
-  end if
-  xrb(j) = abs(crkveb(j))/rb(j)
-  zrb(j) = abs(cikveb(j))/rb(j)
-  tkb(j) = (crkveb(j)**2/sigman2(1,imbb(i))+cikveb(j)**2/sigman2(2,imbb(i)))*half
-  xbb(j) = sigmanq(2,imbb(i))*xrb(j)
-  zbb(j) = sigmanq(1,imbb(i))*zrb(j)
-end do
+if(ibbc == 0) then
+  crkveb(1:napx) = (xv1(1:napx)-clobeam(1,imbb(i))) + parbe(ix,5)
+  cikveb(1:napx) = (xv2(1:napx)-clobeam(2,imbb(i))) + parbe(ix,6)
+else
+  crkveb(1:napx) = ((xv1(1:napx)-clobeam(1,imbb(i))) + parbe(ix,5))*bbcu(imbb(i),11) &
+                 + ((xv2(1:napx)-clobeam(2,imbb(i))) + parbe(ix,6))*bbcu(imbb(i),12)
+  cikveb(1:napx) = ((xv2(1:napx)-clobeam(2,imbb(i))) + parbe(ix,6))*bbcu(imbb(i),11) &
+                 - ((xv1(1:napx)-clobeam(1,imbb(i))) + parbe(ix,5))*bbcu(imbb(i),12)
+end if
+rb(1:napx)  = sqrt(two*(sigman2(2,imbb(i))-sigman2(1,imbb(i))))
+rkb(1:napx) = (strack(i)*pisqrt)/rb(1:napx)
+xrb(1:napx) = abs(crkveb(1:napx))/rb(1:napx)
+zrb(1:napx) = abs(cikveb(1:napx))/rb(1:napx)
+tkb(1:napx) = (crkveb(1:napx)**2/sigman2(1,imbb(i))+cikveb(1:napx)**2/sigman2(2,imbb(i)))*half
+xbb(1:napx) = sigmanq(2,imbb(i))*xrb(1:napx)
+zbb(1:napx) = sigmanq(1,imbb(i))*zrb(1:napx)
 if(ibtyp == 0) then
   do j=1,napx
     call errf(zrb(j),xrb(j),crzb(j),crxb(j))
@@ -32,12 +29,14 @@ do j=1,napx
     yv1(j) = yv1(j)+moidpsv(j)*((rkb(j)*(crzb(j)-exp_mb(-one*tkb(j))*cbzb(j)))*sign(one,crkveb(j))-beamoff(4,imbb(i)))
     yv2(j) = yv2(j)+moidpsv(j)*((rkb(j)*(crxb(j)-exp_mb(-one*tkb(j))*cbxb(j)))*sign(one,cikveb(j))-beamoff(5,imbb(i)))
   else
-    cccc = ((rkb(j)*(crzb(j)-exp_mb(-one*tkb(j))*cbzb(j)))*sign(one,crkveb(j))-beamoff(4,imbb(i)))*bbcu(imbb(i),11)&
-         - ((rkb(j)*(crxb(j)-exp_mb(-one*tkb(j))*cbxb(j)))*sign(one,cikveb(j))-beamoff(5,imbb(i)))*bbcu(imbb(i),12)
-    yv1(j) = yv1(j)+moidpsv(j)*cccc
-    cccc = ((rkb(j)*(crzb(j)-exp_mb(-one*tkb(j))*cbzb(j)))*sign(one,crkveb(j))-beamoff(4,imbb(i)))*bbcu(imbb(i),12)&
-         + ((rkb(j)*(crxb(j)-exp_mb(-one*tkb(j))*cbxb(j)))*sign(one,cikveb(j))-beamoff(5,imbb(i)))*bbcu(imbb(i),11)
-    yv2(j) = yv2(j)+moidpsv(j)*cccc
+    yv1(j) = yv1(j) + moidpsv(j)*( &
+        ((rkb(j)*(crzb(j)-exp_mb(-one*tkb(j))*cbzb(j)))*sign(one,crkveb(j))-beamoff(4,imbb(i)))*bbcu(imbb(i),11) &
+      - ((rkb(j)*(crxb(j)-exp_mb(-one*tkb(j))*cbxb(j)))*sign(one,cikveb(j))-beamoff(5,imbb(i)))*bbcu(imbb(i),12) &
+    )
+    yv2(j) = yv2(j) + moidpsv(j)*( &
+        ((rkb(j)*(crzb(j)-exp_mb(-one*tkb(j))*cbzb(j)))*sign(one,crkveb(j))-beamoff(4,imbb(i)))*bbcu(imbb(i),12) &
+      + ((rkb(j)*(crxb(j)-exp_mb(-one*tkb(j))*cbxb(j)))*sign(one,cikveb(j))-beamoff(5,imbb(i)))*bbcu(imbb(i),11) &
+    )
   end if
 end do
 ! end include/beambeam43.f90
