e96ccd2e2fe918c559dece11047549f2ad4d07ec
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-10-14 15:28:26 +0200

    Added some comments

--- source/track_thin.f90
+++ source/track_thin.f90
@@ -141,9 +141,9 @@ subroutine thin4d(nthinerr)
       select case (ktrack(i))
       case (1)
         stracki=strack(i)
-        if(iexact) then
+        if(iexact) then ! EXACT DRIFT
           do j=1,napx
-            pz     = sqrt(c1e6 - (yv1(j)**2 + yv2(j)**2))*c1m3
+            pz     = sqrt(c1e6 - (yv1(j)**2 + yv2(j)**2))*c1m3 ! pz/p0
             xv1(j) = xv1(j) + stracki*(yv1(j)/pz)
             xv2(j) = xv2(j) + stracki*(yv2(j)/pz)
           end do
@@ -743,18 +743,17 @@ subroutine thin6d(nthinerr)
           ! Collimator is in database, and we're doing collimation
           call collimate_trackThin(stracki,.true.)
         else ! Normal SixTrack drifts
-          if(iexact) then
-            ! EXACT DRIFT
+          if(iexact) then ! EXACT DRIFT
             do j=1,napx
-              pz       = sqrt(c1e6 - (yv1(j)**2 + yv2(j)**2))*c1m3
+              pz       = sqrt(c1e6 - (yv1(j)**2 + yv2(j)**2))*c1m3 ! pz/p0
               xv1(j)   = xv1(j)   + stracki*(yv1(j)/pz)
               xv2(j)   = xv2(j)   + stracki*(yv2(j)/pz)
               sigmv(j) = sigmv(j) + stracki*((one - rvv(j)/pz)*c1e3)
             end do
           else
             do j=1,napx
-              xv1(j)   = xv1(j) + stracki*yv1(j)
-              xv2(j)   = xv2(j) + stracki*yv2(j)
+              xv1(j)   = xv1(j)   + stracki*yv1(j)
+              xv2(j)   = xv2(j)   + stracki*yv2(j)
               sigmv(j) = sigmv(j) + stracki*(c1e3-rvv(j)*(c1e3+(yv1(j)**2+yv2(j)**2)*c5m4))
             end do
           end if
