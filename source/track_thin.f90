d6066355a0b504bade193be3bec085c5c3abc38b
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-10-17 09:27:04 +0200

    Fixed BEAMGAS build

--- source/track_thin.f90
+++ source/track_thin.f90
@@ -662,9 +662,9 @@ subroutine thin6d(nthinerr)
 #ifdef BEAMGAS
       !YIL Call beamGas subroutine whenever a pressure-element is found
       ! should be faster/safer to first check the turn then do the name search
-      if( iturn.eq.1 ) then
-        if (bez(myix)(1:5).eq.'PRESS' .or.  bez(myix)(1:5).eq.'press' ) then
-          call beamGas(myix,secondary,dcum(i),myenom)
+      if(n == 1) then
+        if(bez(myix)(1:5) == "PRESS" .or. bez(myix)(1:5) == "press") then
+          call beamGas(myix,secondary,dcum(i),myenom,n,i)
         end if
       end if
 #endif
