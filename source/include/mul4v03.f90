a57b2f3498adec230336b85c63845559c2cfd97e
tobias <tobias@pcbe15807.dyndns.cern.ch>
2019-10-17 14:10:39 +0200

    Not supposed to be added.

--- source/include/mul4v03.f90
+++ source/include/mul4v03.f90
@@ -1,6 +1,6 @@
 ! start include/mul4v03.f90
   yv1(j)=(yv1(j)+(((strack(i)*zlvj)*moidpsv(j)-dpsv1(j)-omoidpsv(j))*dki(ix,2))*tilts(i))&
          +((c1e3*dki(ix,2))*moidpsv(j))*tilts(i)
-  yv2(j)=(yv2(j)-(((strack(i)*zlvj)*moidpsv(j)-dpsv1(j)-omoidpsv(j))*(-dki(ix,2)))*tiltc(i))&
-         +((c1e3*dki(ix,2))*moidpsv(j))*(one-tiltc(i))
+  yv2(j)=(yv2(j)-(((strack(i)*zlvj)*moidpsv(j)-dpsv1(j)-omoidpsv(j))*dki(ix,2))*tiltc(i))&
+         -((c1e3*dki(ix,2))*moidpsv(j))*(one-tiltc(i))
 ! end include/mul4v03.f90
