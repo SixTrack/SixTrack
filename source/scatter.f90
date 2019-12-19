75176125580edc943556d33951ec6c03254942a8
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-12-19 10:04:07 +0100

    Fix the line counter for scatter summary file when checkpoint/restarting

--- source/scatter.f90
+++ source/scatter.f90
@@ -1960,38 +1960,45 @@ subroutine scatter_initSummaryFile
   write(scatter_sumUnit,"(a)") "#  SCATTER SUMMARY"
   write(scatter_sumUnit,"(a)") "# ================="
   write(scatter_sumUnit,"(a)") "#"
+  nLine = nLine + 4
 #ifdef PYTHIA
   if(scatter_usingPythia) then
     write(scatter_sumUnit,"(a,f16.9,a)") "#  SixTrack Reference Mass: ",nucm0," MeV"
     write(scatter_sumUnit,"(a,f16.9,a)") "#  Pythia Reference Mass:   ",pythia_partMass(1)," MeV"
     write(scatter_sumUnit,"(a)")         "#"
+    nLine = nLine + 3
   end if
 #endif
   write(scatter_sumUnit,"(a,i0,a)") "#  Read ",scatter_nElem," element(s)"
   write(scatter_sumUnit,"(a,i0,a)") "#  Read ",scatter_nPro," profile(s)"
   write(scatter_sumUnit,"(a,i0,a)") "#  Read ",scatter_nGen," generator(s)"
   write(scatter_sumUnit,"(a)")      "#"
-  nLine = nLine + 8
+  nLine = nLine + 4
 
   write(scatter_sumUnit,"(a)") "#  Generators"
   write(scatter_sumUnit,"(a)") "# ============="
+  nLine = nLine + 2
   do iGen=1,scatter_nGen
     write(scatter_sumUnit,"(a,i0,a)") "#  Generator(",iGen,"): '"//trim(scatter_genList(iGen)%genName)//"'"
     nLine = nLine + 1
   end do
   write(scatter_sumUnit,"(a)") "#"
-  nLine = nLine + 3
+  nLine = nLine + 1
 
   write(scatter_sumUnit,"(a)") "#  Profiles"
   write(scatter_sumUnit,"(a)") "# =========="
+  nLine = nLine + 2
   do iPro=1,scatter_nPro
     write(scatter_sumUnit,"(a,i0,a)") "#  Profile(",iPro,"): '"//trim(scatter_proList(iPro)%proName)//"'"
+    nLine = nLine + 1
     select case(scatter_proList(iPro)%proType)
     case(scatter_proGauss1)
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * orbitX,    orbitY    [mm]   =",scatter_proList(iPro)%fParams(4:5)
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * sigmaX,    sigmaY    [mm]   =",scatter_proList(iPro)%fParams(2:3)
+      nLine = nLine + 2
     case(scatter_proBeamRef)
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * orbitX,    orbitY    [mm]   =",scatter_proList(iPro)%fParams(4:5)
+      nLine = nLine + 1
     case(scatter_proBeamUnCorr)
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * betaX,     betaY     [m]    =",scatter_proList(iPro)%fParams(2:3)
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * alphaX,    alphaY    [1]    =",scatter_proList(iPro)%fParams(4:5)
@@ -2000,22 +2007,25 @@ subroutine scatter_initSummaryFile
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * sigmaX,    sigmaY    [mm]   =",scatter_proList(iPro)%fParams(10:11)
       write(scatter_sumUnit,"(a,2(1x,f13.6))") "#   * sigmaXeff, sigmaYeff [mm]   =",scatter_proList(iPro)%fParams(12:13)
       write(scatter_sumUnit,"(a,1x,f13.6)")    "#   * beam2 rotation x,y   [deg]  =",scatter_proList(iPro)%fParams(17)/rad
+      nLine = nLine + 7
     case(scatter_proFlat)
     end select
-    nLine = nLine + 1
   end do
   write(scatter_sumUnit,"(a)") "#"
-  nLine = nLine + 3
+  nLine = nLine + 1
 
   write(scatter_sumUnit,"(a)") "#  Cross Sections"
   write(scatter_sumUnit,"(a)") "# ================"
+  nLine = nLine + 2
   do iElem=1,scatter_nElem
     if(scatter_elemList(iElem)%autoRatio) then
       write(scatter_sumUnit,"(a,i0,a)") "#  Element(",iElem,"): '"//trim(scatter_elemList(iElem)%bezName)//"' "//&
         "[Probability: Auto]"
+      nLine = nLine + 1
     else
       write(scatter_sumUnit,"(a,i0,a,f8.6,a)") "#  Element(",iElem,"): '"//trim(scatter_elemList(iElem)%bezName)//"' "//&
         "[Probability: ",scatter_elemList(iElem)%ratioTot,"]"
+      nLine = nLine + 1
     end if
     do i=1,size(scatter_elemList(iElem)%generatorID)
       iGen = scatter_elemList(iElem)%generatorID(i)
@@ -2024,10 +2034,10 @@ subroutine scatter_initSummaryFile
       nLine = nLine + 1
     end do
     write(scatter_sumUnit,"(a,f15.6,a,f8.6,a)") "#   = Sigma Total:  ",(scatter_elemList(iElem)%sigmaTot*c1e27)," mb [BR: ",one,"]"
-    nLine = nLine + 2
+    nLine = nLine + 1
   end do
   write(scatter_sumUnit,"(a)") "#"
-  nLine = nLine + 3
+  nLine = nLine + 1
 
   write(scatter_sumUnit,"(a)") "#  Summary Log"
   write(scatter_sumUnit,"(a)") "# ============="
