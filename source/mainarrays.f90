7b0dea31758961e916cf1ecd7bcf76e4a465cee9
amereghe <alessio.mereghetti@cern.ch>
2020-01-30 11:23:47 +0100

    following @vkbo suggestions on code style

--- source/mainarrays.f90
+++ source/mainarrays.f90
@@ -316,7 +316,7 @@ subroutine updatePairMap
   pairMap(:,:) = 0
   do j=1,npart
     ! do not update the map in case the particle is not a primary one
-    if (pairID(1,j).eq.0.and.pairID(2,j).eq.0 ) cycle
+    if (pairID(1,j)==0.and.pairID(2,j)==0) cycle
     pairMap(pairID(2,j),pairID(1,j)) = j
   end do
 
