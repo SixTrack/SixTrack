9f6c0da5a23a5d578c620d77fa76a6d3fc8f7d61
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2022-01-02 18:53:40 +0100

    Bump Pythia version to 8.306

--- lib/buildPythia.sh
+++ lib/buildPythia.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=pythia8243
+VERSION=pythia8306
 
 echo ""
 echo " Building PYTHIA support for SixTrack "
@@ -14,7 +14,7 @@ rm -rf pythia*/
 if [ ! -e $VERSION.tgz ]; then
   echo ""
   echo "Downloading:"
-  wget https://pythia.org/download/pythia82/$VERSION.tgz
+  wget https://pythia.org/download/pythia83/$VERSION.tgz
 fi
 
 echo ""
@@ -24,7 +24,7 @@ tar -xf $VERSION.tgz --totals
 echo ""
 echo "Building:"
 cd $VERSION
-./configure --enable-64bit --enable-shared
+./configure
 make
 
 echo ""
