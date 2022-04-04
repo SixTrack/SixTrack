22d5dde493e111bff5d1d3009539d58d4ed95a53
Frederik Van der Veken <frederik@cern.ch>
2022-04-04 22:35:46 +0200

    Release 5.5.0

--- source/version.f90
+++ source/version.f90
@@ -1,7 +1,7 @@
 module mod_version
   ! Keep data type in sync with 'cr_version' and 'cr_moddate'
-  character(len=8),  parameter :: version = "5.4.3"
-  integer,           parameter :: numvers = 50403
-  character(len=10), parameter :: moddate = "2019-12-19"
+  character(len=8),  parameter :: version = "5.5.0"
+  integer,           parameter :: numvers = 50500
+  character(len=10), parameter :: moddate = "2022-04-04"
   character(len=40), parameter :: git_revision = SIXTRACK_GIT_REVISION ! Git hash set by CMake
 end module mod_version
